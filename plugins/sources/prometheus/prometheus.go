01dd29dc614040f16e42fa00e97a901c17cda73a
German Laullon <glaullon@vmware.com>
2019-07-31 00:42:20 +0200

    bucket

--- plugins/sources/prometheus/prometheus.go
+++ plugins/sources/prometheus/prometheus.go
@@ -245,7 +245,8 @@ func (src *prometheusMetricsSource) buildQuantiles(name string, m *dto.Metric, n
 func (src *prometheusMetricsSource) buildHistos(name string, m *dto.Metric, now int64, tags string) []*metrics.MetricPoint {
 	var result []*metrics.MetricPoint
 	for _, b := range m.GetHistogram().Bucket {
-		point := src.metricPoint(name+"."+fmt.Sprint(b.GetUpperBound()), float64(b.GetCumulativeCount()), now, src.source, tags)
+		newTags := fmt.Sprintf("%s bucket=%v", tags, b.GetUpperBound())
+		point := src.metricPoint(name, float64(b.GetCumulativeCount()), now, src.source, newTags)
 		result = src.filterAppend(result, point)
 	}
 	point := src.metricPoint(name+".count", float64(m.GetHistogram().GetSampleCount()), now, src.source, tags)
