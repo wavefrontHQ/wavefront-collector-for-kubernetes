9197e4768d56a7326d07c189bd5f01bc56628cd9
Jake Schuenke <jschuenke@vmware.com>
2021-01-13 10:32:56 -0700

    wip: deploy script update

--- hack/kustomize/deploy.sh
+++ hack/kustomize/deploy.sh
@@ -18,6 +18,13 @@ if [[ -z ${WAVEFRONT_CLUSTER} || -z ${API_TOKEN} ]] ; then
     exit 1
 fi
 
+if [[ -z ${VERSION} ]] ; then
+    VERSION=${DEFAULT_VERSION}
+fi
+
+NAMESPACE_VERSION=$(echo "$VERSION" | tr . -)
+echo $NAMESPACE_VERSION
+
 #TODO: change the user provided cluster name that shows up in Wavefront
 
 # TODO: input the base image
@@ -31,17 +38,13 @@ OVERLAYS_DIR="overlays"
 #TODO: need to replace the kustomize template to source from temp directory
 
 sed "s/YOUR_CLUSTER/${WAVEFRONT_CLUSTER}/g; s/YOUR_API_TOKEN/${API_TOKEN}/g" ${BASE_DIR}/proxy.template.yaml > ${BASE_DIR}/proxy.yaml
-
-if [[ -z ${VERSION} ]] ; then
-    VERSION=${DEFAULT_VERSION}
-fi
+sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml > ${BASE_DIR}/kustomization.yaml
+sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/0-collector-namespace.template.yaml > ${BASE_DIR}/0-collector-namespace.yaml
 
 sed "s/YOUR_IMAGE_TAG/${VERSION}/g" ${OVERLAYS_DIR}/versions/kustomization.template.yaml > ${OVERLAYS_DIR}/versions/kustomization.yaml
 sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/config/collector.yaml.template > ${OVERLAYS_DIR}/test/config/collector.yaml
 
-cat ${BASE_DIR}/proxy.yaml
-cat ${OVERLAYS_DIR}/versions/kustomization.yaml
-
-#TODO: figure out how to deploy to a cluster and which cluster
+# cat ${BASE_DIR}/proxy.yaml
+# cat ${OVERLAYS_DIR}/versions/kustomization.yaml
 
 kubectl apply -k ${OVERLAYS_DIR}/test/config/
