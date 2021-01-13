e210f68a49e8ee6139d6ec1d81021ce9d7612457
Jake Schuenke <jschuenke@vmware.com>
2021-01-13 15:32:28 -0700

    Refactored file strucuture to fit kustomize conventions, got namespaces based on version working

--- hack/kustomize/deploy.sh
+++ hack/kustomize/deploy.sh
@@ -23,9 +23,6 @@ if [[ -z ${VERSION} ]] ; then
 fi
 
 NAMESPACE_VERSION=$(echo "$VERSION" | tr . -)
-echo $NAMESPACE_VERSION
-
-#TODO: change the user provided cluster name that shows up in Wavefront
 
 # TODO: input the base image
 # input the diff image
@@ -37,14 +34,10 @@ OVERLAYS_DIR="overlays"
 #TODO: temp directory for intermediate files
 #TODO: need to replace the kustomize template to source from temp directory
 
+#TODO: Migrate these sed to use kustomize edit
 sed "s/YOUR_CLUSTER/${WAVEFRONT_CLUSTER}/g; s/YOUR_API_TOKEN/${API_TOKEN}/g" ${BASE_DIR}/proxy.template.yaml > ${BASE_DIR}/proxy.yaml
-sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml > ${BASE_DIR}/kustomization.yaml
-sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/0-collector-namespace.template.yaml > ${BASE_DIR}/0-collector-namespace.yaml
+sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml | sed "s/YOUR_IMAGE_TAG/${VERSION}/g" > ${BASE_DIR}/kustomization.yaml
 
-sed "s/YOUR_IMAGE_TAG/${VERSION}/g" ${OVERLAYS_DIR}/versions/kustomization.template.yaml > ${OVERLAYS_DIR}/versions/kustomization.yaml
-sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/config/collector.yaml.template > ${OVERLAYS_DIR}/test/config/collector.yaml
+sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/collector.yaml.template | sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" > ${OVERLAYS_DIR}/test/collector.yaml
 
-# cat ${BASE_DIR}/proxy.yaml
-# cat ${OVERLAYS_DIR}/versions/kustomization.yaml
-
-kubectl apply -k ${OVERLAYS_DIR}/test/config/
+kustomize build ${OVERLAYS_DIR}/test | kubectl apply -f -
