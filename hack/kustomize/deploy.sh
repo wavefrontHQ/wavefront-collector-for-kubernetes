387e45661573a62f2421ed4f6ba6b79338b3ce85
Vikram Raman <vikram@wavefront.com>
2021-01-14 16:45:29 -0800

    basic prometheus diff

--- hack/kustomize/deploy.sh
+++ hack/kustomize/deploy.sh
@@ -9,9 +9,14 @@
 DEFAULT_IMAGE_NAME="wavefronthq\/wavefront-kubernetes-collector"
 DEFAULT_VERSION="1.2.6"
 
+#TODO: use getopts instead?
 WAVEFRONT_CLUSTER=$1
 API_TOKEN=$2
 VERSION=$3
+IMAGE=$4
+PROFILE=$5
+
+echo "$WAVEFRONT_CLUSTER $API_TOKEN $VERSION $IMAGE $PROFILE"
 
 if [[ -z ${WAVEFRONT_CLUSTER} || -z ${API_TOKEN} ]] ; then
     #TODO: source these from environment variables if not provided
@@ -22,23 +27,28 @@ fi
 if [[ -z ${VERSION} ]] ; then
     VERSION=${DEFAULT_VERSION}
 fi
-
 NAMESPACE_VERSION=$(echo "$VERSION" | tr . -)
 
-# TODO: input the base image
-# input the diff image
-IMAGE_NAME=${DEFAULT_IMAGE_NAME}
+if [[ -z ${IMAGE} ]] ; then
+    IMAGE=${DEFAULT_IMAGE_NAME}
+fi
 
 BASE_DIR="base"
 OVERLAYS_DIR="overlays"
+
+if [[ -z ${PROFILE} ]] ; then
+    PROFILE=${OVERLAYS_DIR}/test
+else
+    PROFILE=${OVERLAYS_DIR}/${PROFILE}
+fi
 
 #TODO: temp directory for intermediate files
 #TODO: need to replace the kustomize template to source from temp directory
 
 #TODO: Migrate these sed to use kustomize edit
 sed "s/YOUR_CLUSTER/${WAVEFRONT_CLUSTER}/g; s/YOUR_API_TOKEN/${API_TOKEN}/g" ${BASE_DIR}/proxy.template.yaml > ${BASE_DIR}/proxy.yaml
-sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml | sed "s/YOUR_IMAGE_TAG/${VERSION}/g" | sed "s/YOUR_IMAGE_NAME/${IMAGE_NAME}/g" > ${BASE_DIR}/kustomization.yaml
+sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml | sed "s/YOUR_IMAGE_TAG/${VERSION}/g" | sed "s/YOUR_IMAGE_NAME/${IMAGE}/g" > ${BASE_DIR}/kustomization.yaml
 
 sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/collector.yaml.template | sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" > ${OVERLAYS_DIR}/test/collector.yaml
 
-kustomize build ${OVERLAYS_DIR}/test | kubectl apply -f -
+kustomize build ${PROFILE} | kubectl apply -f -
--- hack/kustomize/test.sh
+++ hack/kustomize/test.sh
@@ -1,65 +1,42 @@
 #! /bin/bash
 
-# This script automates the deployment of the collector to a specific k8s cluster
-
-# 1. VERSION (OPTIONAL)
-# 2. NAMESPACE (OPTIONAL)
-# 3. IMAGE(OPTIONAL) -- if missing build from source
+# This script automates the functional testing of the collector
 
 DEFAULT_VERSION="1.2.7-beta1"
 DEFAULT_IMAGE_NAME="vikramraman\/wavefront-collector"
 
 WAVEFRONT_CLUSTER=$1
 API_TOKEN=$2
-VERSION=$3
 
 LOG_FILE="/tmp/logs.txt"
+SORTED_FILE="/tmp/sorted.txt"
 
-if [[ -z ${WAVEFRONT_CLUSTER} || -z ${API_TOKEN} ]] ; then
-    #TODO: source these from environment variables if not provided
-    echo "wavefront cluster and token required"
-    exit 1
-fi
-
-if [[ -z ${VERSION} ]] ; then
-    VERSION=${DEFAULT_VERSION}
-fi
-
-NAMESPACE_VERSION=$(echo "$VERSION" | tr . -)
-NS=${NAMESPACE_VERSION}-wavefront-collector
-
-# TODO: input the base image
-# input the diff image
-IMAGE_NAME=${DEFAULT_IMAGE_NAME}
-
-BASE_DIR="base"
-OVERLAYS_DIR="overlays"
-
-#TODO: temp directory for intermediate files
-#TODO: need to replace the kustomize template to source from temp directory
-
-sed "s/YOUR_CLUSTER/${WAVEFRONT_CLUSTER}/g; s/YOUR_API_TOKEN/${API_TOKEN}/g" ${BASE_DIR}/proxy.template.yaml > ${BASE_DIR}/proxy.yaml
-
-sed "s/NAMESPACE/${NS}/g" ${BASE_DIR}/kustomization.template.yaml | sed "s/YOUR_IMAGE_TAG/${VERSION}/g" | sed "s/YOUR_IMAGE_NAME/${IMAGE_NAME}/g" > ${BASE_DIR}/kustomization.yaml
-
-#sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/config/collector.yaml.template > ${OVERLAYS_DIR}/test/config/collector.yaml
-
-cat ${BASE_DIR}/proxy.yaml
-cat ${OVERLAYS_DIR}/versions/kustomization.yaml
-
-kustomize build ${OVERLAYS_DIR}/flushonce | kubectl apply -f -
-
-# emit the output out to a log dump and then diff becomes easy
-#TODO: diff the metrics
+echo "deploying collector ${DEFAULT_IMAGE_NAME} ${DEFAULT_VERSION}"
+./deploy.sh ${WAVEFRONT_CLUSTER} ${API_TOKEN} ${DEFAULT_VERSION} ${DEFAULT_IMAGE_NAME} flushonce
 
 echo "waiting for logs to be emitted"
 sleep 30
+
+NAMESPACE_VERSION=$(echo "${DEFAULT_VERSION}" | tr . -)
+NS=${NAMESPACE_VERSION}-wavefront-collector
 
 PODS=`kubectl -n ${NS} get pod -l k8s-app=wavefront-collector | awk '{print $1}' | tail +2`
 
 rm -f ${LOG_FILE}
 
+# TODO: relies on the prefix from the sample app to isolate prom metrics
+PROM_PREFIX="prom-example."
+
 for pod in ${PODS} ; do
     echo "writing metrics from ${pod} into ${LOG_FILE}"
-    kubectl logs ${pod} -n ${NS} | grep 'Metric:' | jq -r '.msg' >> ${LOG_FILE}
+    kubectl logs ${pod} -n ${NS} | grep 'Metric: prom-example' | jq -r '.msg' >> ${LOG_FILE}
 done
+
+sort ${LOG_FILE} > ${SORTED_FILE}
+
+echo "diff between prom files"
+echo ${SORTED_FILE}
+diff -q ${SORTED_FILE} files/prometheus-baseline.txt
+if [[ $? -eq 0 ]] ; then
+   echo "diff succeeded"
+fi
