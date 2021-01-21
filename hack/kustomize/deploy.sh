1cfd1e6b36b1bacb998e926215dbb3e346497afa
Mark Michael <marklmichael@gmail.com>
2021-01-21 11:08:19 -0700

    Deploy and test scripts MVP (#86)
    Co-authored by Jake Schuenke, Mark Michaels and Joe Gee.
    
    * minor doc tweak
    
    * init kustomizations
    
    * added prom parser and versions
    
    * added deploy script and config overlay
    
    * remove release script
    
    * Refactored file strucuture to fit kustomize conventions, got namespaces based on version working
    
    * prelim diff script
    
    * basic prometheus diff
    
    * refactored diff/deploy scripts
    
    * refactored to use sed for collector config templatizing
    
    * remove certain tags from comparison, add redirect log flag, add ability to pass in image name and version
    
    * refactored test and deploy to only pass in flush_once flag
    
    Co-authored-by: Vikram Raman <vikram@wavefront.com>
    Co-authored-by: Jake Schuenke <jschuenke@vmware.com>

---
+++ hack/kustomize/deploy.sh
@@ -0,0 +1,102 @@
+#! /bin/bash -e
+
+# This script automates the deployment of the collector to a specific k8s cluster
+
+DEFAULT_IMAGE_NAME="wavefronthq\/wavefront-kubernetes-collector"
+DEFAULT_VERSION="1.2.6"
+DEFAULT_USE_CLASSIC_PROMETHEUS=false
+DEFAULT_FLUSH_ONCE=false
+
+if [[ -z ${FLUSH_ONCE} ]] ; then
+    FLUSH_ONCE=${DEFAULT_FLUSH_ONCE}
+fi
+
+if $FLUSH_ONCE ;
+  then
+    REDIRECT_TO_LOG=true
+    FLUSH_INTERVAL=15
+    COLLECTION_INTERVAL=10
+  else
+    REDIRECT_TO_LOG=false
+    FLUSH_INTERVAL=30
+    COLLECTION_INTERVAL=60
+fi
+
+if [[ -z ${USE_CLASSIC_PROMETHEUS} ]] ; then
+    USE_CLASSIC_PROMETHEUS=${DEFAULT_USE_CLASSIC_PROMETHEUS}
+fi
+
+function print_usage_and_exit() {
+    echo "Failure: $1"
+    echo "Usage: $0 [flags] [options]"
+    echo -e "\t-c wavefront instance name (required)"
+    echo -e "\t-t wavefront token (required)"
+    echo -e "\t-i collector docker image name"
+    echo -e "\t-v collector docker image version"
+    echo -e "\t-p kustomize overlay to deploy"
+    exit 1
+}
+
+WF_CLUSTER=
+WF_TOKEN=
+VERSION=
+IMAGE=
+PROFILE=
+
+while getopts "c:t:v:i:p:" opt; do
+  case $opt in
+    c)
+      WF_CLUSTER="$OPTARG"
+      ;;
+    t)
+      WF_TOKEN="$OPTARG"
+      ;;
+    v)
+      VERSION="$OPTARG"
+      ;;
+    i)
+      IMAGE="$OPTARG"
+      ;;
+    p)
+      PROFILE="$OPTARG"
+      ;;
+    \?)
+      print_usage_and_exit "Invalid option: -$OPTARG"
+      ;;
+  esac
+done
+
+echo "$WF_CLUSTER $VERSION $IMAGE $PROFILE"
+
+if [[ -z ${WF_CLUSTER} || -z ${WF_TOKEN} ]] ; then
+    #TODO: source these from environment variables if not provided
+    print_usage_and_exit "wavefront instance and token required"
+fi
+
+BASE_DIR="base"
+OVERLAYS_DIR="overlays"
+
+if [[ -z ${VERSION} ]] ; then
+    VERSION=${DEFAULT_VERSION}
+fi
+NAMESPACE_VERSION=$(echo "$VERSION" | tr . -)
+
+if [[ -z ${IMAGE} ]] ; then
+    IMAGE=${DEFAULT_IMAGE_NAME}
+fi
+
+#TODO: temp directory for intermediate files
+#TODO: need to replace the kustomize template to source from temp directory
+
+#TODO: Migrate these sed to use kustomize edit
+sed "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WF_TOKEN}/g" ${BASE_DIR}/proxy.template.yaml > ${BASE_DIR}/proxy.yaml
+sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" ${BASE_DIR}/kustomization.template.yaml | sed "s/YOUR_IMAGE_TAG/${VERSION}/g" | sed "s/YOUR_IMAGE_NAME/${IMAGE}/g" > ${BASE_DIR}/kustomization.yaml
+
+sed "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" ${OVERLAYS_DIR}/test/collector.yaml.template | sed "s/NAMESPACE/${NAMESPACE_VERSION}-wavefront-collector/g" \
+|  sed "s/FLUSH_ONCE/${FLUSH_ONCE}/g" \
+|  sed "s/REDIRECT_TO_LOG/${REDIRECT_TO_LOG}/g" \
+|  sed "s/FLUSH_INTERVAL/${FLUSH_INTERVAL}/g" \
+|  sed "s/COLLECTION_INTERVAL/${COLLECTION_INTERVAL}/g" \
+|  sed "s/USE_CLASSIC_PROMETHEUS/${USE_CLASSIC_PROMETHEUS}/g" > ${OVERLAYS_DIR}/test/collector.yaml \
+
+kustomize build ${OVERLAYS_DIR}/test | kubectl apply -f -
---
+++ hack/kustomize/test.sh
@@ -0,0 +1,94 @@
+#! /bin/bash -e
+
+# This script automates the functional testing of the collector
+
+DEFAULT_VERSION="1.2.6"
+DEFAULT_IMAGE_NAME="wavefronthq\/wavefront-collector"
+
+WAVEFRONT_CLUSTER=$1
+API_TOKEN=$2
+VERSION=$3
+IMAGE_NAME=$4
+
+
+OUT_DIR=/tmp
+PROM_DUMP=${OUT_DIR}/prom.txt
+SORTED_FILE=${OUT_DIR}/sorted.txt
+
+if [[ -z ${VERSION} ]] ; then
+    VERSION=${DEFAULT_VERSION}
+fi
+
+if [[ -z ${IMAGE_NAME} ]] ; then
+    IMAGE_NAME=${DEFAULT_IMAGE_NAME}
+fi
+
+function print_msg_and_exit() {
+    echo -e "$1"
+    exit 1
+}
+
+# dumps metrics from a pod into a log file for a given prefix
+function dump_metrics() {
+    POD=$1
+    PREFIX=$2
+    OUT=$3
+    NAMESPACE=$4
+    
+    echo "capturing ${PREFIX} metrics from ${NS}:${POD} into ${OUT}"
+    kubectl logs ${POD} -n ${NAMESPACE} | grep "Metric: ${PREFIX}" | jq -r '.msg' >> ${OUT}
+}
+
+# validates metrics against a golden copy
+function validate_metrics() {
+    TYPE=$1
+    DUMP=$2
+    BASELINE=$3
+
+    sort ${DUMP} > ${SORTED_FILE}
+
+    echo "validating ${TYPE} metrics"
+
+    diff -q ${SORTED_FILE} ${BASELINE}
+    if [[ $? -eq 0 ]] ; then
+       echo "${TYPE} validation succeeded"
+    else
+       echo "${TYPE} validation failed"
+    fi
+}
+
+function cleanup() {
+    rm -f ${PROM_DUMP}
+    #TODO: cleanup other files here
+}
+
+echo "deploying collector ${IMAGE_NAME} ${VERSION}"
+
+env FLUSH_ONCE=true \
+USE_CLASSIC_PROMETHEUS=false \
+./deploy.sh -c ${WAVEFRONT_CLUSTER} -t ${API_TOKEN} -v ${VERSION} -i ${IMAGE_NAME}
+
+echo "waiting for logs..."
+sleep 30
+
+NAMESPACE_VERSION=$(echo "${VERSION}" | tr . -)
+NS=${NAMESPACE_VERSION}-wavefront-collector
+
+PODS=`kubectl -n ${NS} get pod -l k8s-app=wavefront-collector | awk '{print $1}' | tail +2`
+if [[ -z ${PODS} ]] ; then
+    print_msg_and_exit "no collector pods found"
+fi
+
+# cleanup existing dumps
+cleanup
+
+# TODO: relies on the prefix from the sample app to isolate prom metrics
+PROM_PREFIX="prom-example."
+
+for pod in ${PODS} ; do
+    dump_metrics ${pod} ${PROM_PREFIX} ${PROM_DUMP} ${NS}
+    #TODO: dump_metrics for other prefixes for diff metric sources
+done
+
+validate_metrics prometheus ${PROM_DUMP} files/prometheus-baseline.txt
+#TODO: add validation for other metric sources
