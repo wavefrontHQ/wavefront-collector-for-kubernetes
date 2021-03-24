53990a9ebce35684b919c0491cb8be435f464c36
Joe Gee <jgee@vmware.com>
2021-03-24 14:55:14 -0600

    Rename receiver variable
    Signed-off-by: Jacob Schuenke <jschuenke@vmware.com>

--- plugins/sources/prometheus/point_builder.go
+++ plugins/sources/prometheus/point_builder.go
@@ -36,7 +36,7 @@ func NewPointBuilder(src *prometheusMetricsSource) *pointBuilder {
 
 }
 
-func (pb *pointBuilder) build(metricFamilies map[string]*dto.MetricFamily) ([]*metrics.MetricPoint, error) {
+func (builder *pointBuilder) build(metricFamilies map[string]*dto.MetricFamily) ([]*metrics.MetricPoint, error) {
 	now := time.Now().Unix()
 	var result []*metrics.MetricPoint
 
@@ -44,11 +44,11 @@ func (pb *pointBuilder) build(metricFamilies map[string]*dto.MetricFamily) ([]*m
 		for _, m := range mf.Metric {
 			var points []*metrics.MetricPoint
 			if mf.GetType() == dto.MetricType_SUMMARY {
-				points = pb.buildSummaryPoints(metricName, m, now, pb.buildTags(m))
+				points = builder.buildSummaryPoints(metricName, m, now, builder.buildTags(m))
 			} else if mf.GetType() == dto.MetricType_HISTOGRAM {
-				points = pb.buildHistogramPoints(metricName, m, now, pb.buildTags(m))
+				points = builder.buildHistogramPoints(metricName, m, now, builder.buildTags(m))
 			} else {
-				points = pb.buildPoints(metricName, m, now)
+				points = builder.buildPoints(metricName, m, now)
 			}
 
 			if len(points) > 0 {
@@ -59,86 +59,86 @@ func (pb *pointBuilder) build(metricFamilies map[string]*dto.MetricFamily) ([]*m
 	return result, nil
 }
 
-func (pb *pointBuilder) metricPoint(name string, value float64, ts int64, source string, tags map[string]string) *metrics.MetricPoint {
+func (builder *pointBuilder) metricPoint(name string, value float64, ts int64, source string, tags map[string]string) *metrics.MetricPoint {
 	point := &metrics.MetricPoint{
-		Metric:    pb.prefix + strings.Replace(name, "_", ".", -1),
+		Metric:    builder.prefix + strings.Replace(name, "_", ".", -1),
 		Value:     value,
 		Timestamp: ts,
 		Source:    source,
 	}
-	point.SetLabelPairs(pb.deduplicate(tags)) //store tags as LabelPairs for memory optimization
+	point.SetLabelPairs(builder.deduplicate(tags)) //store tags as LabelPairs for memory optimization
 	return point
 }
 
-func (pb *pointBuilder) filterAppend(slice []*metrics.MetricPoint, point *metrics.MetricPoint) []*metrics.MetricPoint {
-	if pb.isValidMetric(point.Metric, point.GetTags()) {
+func (builder *pointBuilder) filterAppend(slice []*metrics.MetricPoint, point *metrics.MetricPoint) []*metrics.MetricPoint {
+	if builder.isValidMetric(point.Metric, point.GetTags()) {
 		return append(slice, point)
 	}
 	return slice
 }
 
 // Get name and value from metric
-func (pb *pointBuilder) buildPoints(name string, m *dto.Metric, now int64) []*metrics.MetricPoint {
+func (builder *pointBuilder) buildPoints(name string, m *dto.Metric, now int64) []*metrics.MetricPoint {
 	var result []*metrics.MetricPoint
 	if m.Gauge != nil {
 		if !math.IsNaN(m.GetGauge().GetValue()) {
-			point := pb.metricPoint(name+".gauge", m.GetGauge().GetValue(), now, pb.source, pb.buildTags(m))
-			result = pb.filterAppend(result, point)
+			point := builder.metricPoint(name+".gauge", m.GetGauge().GetValue(), now, builder.source, builder.buildTags(m))
+			result = builder.filterAppend(result, point)
 		}
 	} else if m.Counter != nil {
 		if !math.IsNaN(m.GetCounter().GetValue()) {
-			point := pb.metricPoint(name+".counter", m.GetCounter().GetValue(), now, pb.source, pb.buildTags(m))
-			result = pb.filterAppend(result, point)
+			point := builder.metricPoint(name+".counter", m.GetCounter().GetValue(), now, builder.source, builder.buildTags(m))
+			result = builder.filterAppend(result, point)
 		}
 	} else if m.Untyped != nil {
 		if !math.IsNaN(m.GetUntyped().GetValue()) {
-			point := pb.metricPoint(name+".value", m.GetUntyped().GetValue(), now, pb.source, pb.buildTags(m))
-			result = pb.filterAppend(result, point)
+			point := builder.metricPoint(name+".value", m.GetUntyped().GetValue(), now, builder.source, builder.buildTags(m))
+			result = builder.filterAppend(result, point)
 		}
 	}
 	return result
 }
 
 // Get Quantiles from summary metric
-func (pb *pointBuilder) buildSummaryPoints(name string, m *dto.Metric, now int64, tags map[string]string) []*metrics.MetricPoint {
+func (builder *pointBuilder) buildSummaryPoints(name string, m *dto.Metric, now int64, tags map[string]string) []*metrics.MetricPoint {
 	var result []*metrics.MetricPoint
 	for _, q := range m.GetSummary().Quantile {
 		if !math.IsNaN(q.GetValue()) {
 			newTags := copyOf(tags)
 			newTags["quantile"] = fmt.Sprintf("%v", q.GetQuantile())
-			point := pb.metricPoint(name, q.GetValue(), now, pb.source, newTags)
-			result = pb.filterAppend(result, point)
+			point := builder.metricPoint(name, q.GetValue(), now, builder.source, newTags)
+			result = builder.filterAppend(result, point)
 		}
 	}
-	point := pb.metricPoint(name+".count", float64(m.GetSummary().GetSampleCount()), now, pb.source, tags)
-	result = pb.filterAppend(result, point)
-	point = pb.metricPoint(name+".sum", m.GetSummary().GetSampleSum(), now, pb.source, tags)
-	result = pb.filterAppend(result, point)
+	point := builder.metricPoint(name+".count", float64(m.GetSummary().GetSampleCount()), now, builder.source, tags)
+	result = builder.filterAppend(result, point)
+	point = builder.metricPoint(name+".sum", m.GetSummary().GetSampleSum(), now, builder.source, tags)
+	result = builder.filterAppend(result, point)
 
 	return result
 }
 
 // Get Buckets from histogram metric
-func (pb *pointBuilder) buildHistogramPoints(name string, m *dto.Metric, now int64, tags map[string]string) []*metrics.MetricPoint {
+func (builder *pointBuilder) buildHistogramPoints(name string, m *dto.Metric, now int64, tags map[string]string) []*metrics.MetricPoint {
 	var result []*metrics.MetricPoint
-	histName := pb.histogramName(name)
+	histName := builder.histogramName(name)
 	for _, b := range m.GetHistogram().Bucket {
 		newTags := copyOf(tags)
 		newTags["le"] = fmt.Sprintf("%v", b.GetUpperBound())
-		point := pb.metricPoint(histName, float64(b.GetCumulativeCount()), now, pb.source, newTags)
-		result = pb.filterAppend(result, point)
+		point := builder.metricPoint(histName, float64(b.GetCumulativeCount()), now, builder.source, newTags)
+		result = builder.filterAppend(result, point)
 	}
-	point := pb.metricPoint(name+".count", float64(m.GetHistogram().GetSampleCount()), now, pb.source, tags)
-	result = pb.filterAppend(result, point)
-	point = pb.metricPoint(name+".sum", m.GetHistogram().GetSampleSum(), now, pb.source, tags)
-	result = pb.filterAppend(result, point)
+	point := builder.metricPoint(name+".count", float64(m.GetHistogram().GetSampleCount()), now, builder.source, tags)
+	result = builder.filterAppend(result, point)
+	point = builder.metricPoint(name+".sum", m.GetHistogram().GetSampleSum(), now, builder.source, tags)
+	result = builder.filterAppend(result, point)
 	return result
 }
 
 // Get labels from metric
-func (pb *pointBuilder) buildTags(m *dto.Metric) map[string]string {
-	tags := make(map[string]string, len(pb.tags)+len(m.Label))
-	for k, v := range pb.tags {
+func (builder *pointBuilder) buildTags(m *dto.Metric) map[string]string {
+	tags := make(map[string]string, len(builder.tags)+len(m.Label))
+	for k, v := range builder.tags {
 		if len(v) > 0 {
 			tags[k] = v
 		}
@@ -153,19 +153,19 @@ func (pb *pointBuilder) buildTags(m *dto.Metric) map[string]string {
 	return tags
 }
 
-func (pb *pointBuilder) histogramName(name string) string {
-	if pb.omitBucketSuffix {
+func (builder *pointBuilder) histogramName(name string) string {
+	if builder.omitBucketSuffix {
 		return name
 	}
 	return name + ".bucket"
 }
 
-func (pb *pointBuilder) deduplicate(tags map[string]string) []metrics.LabelPair {
+func (builder *pointBuilder) deduplicate(tags map[string]string) []metrics.LabelPair {
 	result := make([]metrics.LabelPair, 0)
 	for k, v := range tags {
 		result = append(result, metrics.LabelPair{
-			Name:  pb.interner.Intern(k),
-			Value: pb.interner.Intern(v),
+			Name:  builder.interner.Intern(k),
+			Value: builder.interner.Intern(v),
 		})
 	}
 	return result
