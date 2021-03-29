dc486aba11c7febb728cc52830bf929bf94fec22
Jake Schuenke <jschuenke@vmware.com>
2021-03-29 16:14:30 -0600

    wip

--- release/bump-version.sh
+++ release/bump-version.sh
@@ -9,11 +9,11 @@ DEPLOY_DIR=../deploy
 TMP_FILE=/tmp/temporary
 
 ## Bump to current version
-sed "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${DEPLOY_DIR}/kubernetes/5-collector-daemonset.yaml > ${TMP_FILE} && mv ${TMP_FILE} ${DEPLOY_DIR}/kubernetes/5-collector-daemonset.yaml
-sed "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${DEPLOY_DIR}/openshift/collector/3-collector-deployment.yaml > ${TMP_FILE} && mv ${TMP_FILE} ${DEPLOY_DIR}/openshift/collector/3-collector-deployment.yaml
-sed "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${KUSTOMIZE_DIR}/base/5-collector-daemonset.yaml > ${TMP_FILE} && mv ${TMP_FILE} ${KUSTOMIZE_DIR}/base/5-collector-daemonset.yaml
-sed "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${KUSTOMIZE_DIR}/deploy.sh > ${TMP_FILE} && mv ${TMP_FILE} ${KUSTOMIZE_DIR}/deploy.sh
+sed -i "" "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${DEPLOY_DIR}/kubernetes/5-collector-daemonset.yaml
+sed -i "" "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${DEPLOY_DIR}/openshift/collector/3-collector-deployment.yaml
+sed -i "" "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${KUSTOMIZE_DIR}/base/5-collector-daemonset.yaml
+sed -i "" "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ${KUSTOMIZE_DIR}/deploy.sh
 
 ## Bump to future version
-sed "s/${CURRENT_VERSION}/${NEXT_VERSION}/g" ../Makefile > ${TMP_FILE} && mv ${TMP_FILE} ../Makefile
-sed "s/${CURRENT_VERSION}/${NEXT_VERSION}/g" ${KUSTOMIZE_DIR}/test.sh > ${TMP_FILE} && mv ${TMP_FILE} ${KUSTOMIZE_DIR}/test.sh
+sed -i "" "s/${CURRENT_VERSION}/${NEXT_VERSION}/g" ../Makefile
+sed -i "" "s/${CURRENT_VERSION}/${NEXT_VERSION}/g" ${KUSTOMIZE_DIR}/test.sh
