055a3a5928674c9dc4bb9be45050ef75a8cc64d2
Mark Michael & Jake Schuenke <mamichael@vmware.com>
2021-04-07 14:08:06 -0600

    updated get metric names script

--- hack/get-metric-names.sh
+++ hack/get-metric-names.sh
@@ -3,11 +3,24 @@ set -e -u -o pipefail
 
 NS=$(kubectl get namespaces | awk '/wavefront-collector/ {print $1}')
 COLLECTOR=$(kubectl get pods -n $NS  | awk '/wavefront-collector-/ {print $1}')
-echo "Gonna sleep for a bit..."
-for i in `seq 90`; do
-    echo -n "."
-    sleep 10
+
+
+PODS=`kubectl -n ${NS} get pod -l k8s-app=wavefront-collector | awk '{print $1}' | tail +2`
+
+echo $PODS
+#echo "Gonna sleep for a bit..."
+#for i in `seq 30`; do
+#    echo -n "."
+#    sleep 10
+#done
+
+rm -f sorted-metrics.csv unsorted-metrics.csv
+for pod in ${PODS} ; do
+  echo "Ok, grabbing logs from pod ${pod}"
+  kubectl logs $pod -n $NS --since=5m | awk '/Metric/ {print $2}' >>unsorted-metrics.csv
 done
-echo "Ok, grabbing logs..."
-kubectl logs $COLLECTOR -n $NS --since=5m | awk '/Metric/ {print $2}' >unsorted-metrics.csv
+
 sort -u <unsorted-metrics.csv >sorted-metrics.csv
+rm -f unsorted-metrics.csv
+
+
