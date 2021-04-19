c17750d2353fdb8f8a53dae1387b2bc0a2396d7b
Mark Michael <mamichael@vmware.com>
2021-04-19 14:48:20 -0600

    scale time using floating point numbers, inject now into intervalsMissed
    Signed-off-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- plugins/sources/interval_timer.go
+++ plugins/sources/interval_timer.go
@@ -1,6 +1,8 @@
 package sources
 
-import "time"
+import (
+	"time"
+)
 
 type IntervalTimer struct {
 	*time.Timer
@@ -10,40 +12,39 @@ type IntervalTimer struct {
 }
 
 func (t *IntervalTimer) Reset() (intervalsMissed int64) {
-	intervals := t.intervalsMissed()
-	nowTime := time.Now()
-	t.lastResetTime = nowTime
-	diff := nowTime.Sub(t.startTime)
-	waitTime := t.waitToNextInterval(diff)
+	now := time.Now()
+	intervals := t.intervalsMissed(now)
+	t.lastResetTime = now
+	waitTime := t.waitToNextInterval(now.Sub(t.startTime))
 	t.Timer.Reset(waitTime)
 	return intervals
 }
 
 func NewIntervalTimer(interval time.Duration) *IntervalTimer {
+	now := time.Now()
 	return &IntervalTimer{
-		Timer:     time.NewTimer(interval),
-		interval:  interval,
-		startTime: time.Now(),
+		Timer:         time.NewTimer(interval),
+		interval:      interval,
+		startTime:     now,
+		lastResetTime: now,
 	}
 }
 
-func (t *IntervalTimer) intervalsMissed() (intervalsMissed int64) {
-	nowTime := time.Now()
-	if t.lastResetTime.IsZero() || nowTime.Sub(t.lastResetTime) < t.interval {
+func (t *IntervalTimer) intervalsMissed(now time.Time) (intervalsMissed int64) {
+	if now.Sub(t.lastResetTime) < t.interval {
 		return 0
 	}
-	return int64((nowTime.Sub(t.lastResetTime) / t.interval) - 1)
+	return int64((now.Sub(t.lastResetTime) / t.interval) - 1)
 }
 
 func (t *IntervalTimer) waitToNextInterval(diff time.Duration) time.Duration {
 	wait := t.interval - (diff % t.interval)
-	per10K := 333 // 3.33%. This was chosen arbitrarily. If you have a better idea, change it!
-	if wait < scaleInterval(t.interval, per10K) {
+	if wait < scaleInterval(t.interval, 0.0333) { // 3.33%. This was chosen arbitrarily. If you have a better idea, change it!
 		wait += t.interval
 	}
 	return wait
 }
 
-func scaleInterval(interval time.Duration, per10K int) time.Duration {
-	return (interval*time.Duration(per10K) + 10_000 - 1) / 10_000
+func scaleInterval(interval time.Duration, ratio float64) time.Duration {
+	return time.Duration(float64(interval) * ratio)
 }
--- plugins/sources/interval_timer_test.go
+++ plugins/sources/interval_timer_test.go
@@ -43,33 +43,33 @@ func TestReset(t *testing.T) {
 }
 
 func TestMissedCount(t *testing.T) {
-	t.Run("zero time", func(t *testing.T) {
+	now := time.Now()
+	t.Run("no resets", func(t *testing.T) {
 		timer := NewIntervalTimer(15 * time.Second)
-		assert.Equal(t, int64(0), timer.intervalsMissed())
+		assert.Equal(t, int64(0), timer.intervalsMissed(time.Now()))
 	})
 
 	t.Run("first interval", func(t *testing.T) {
 		timer := NewIntervalTimer(15 * time.Second)
-		timer.lastResetTime = time.Now().Add(-15 * time.Second)
-		assert.Equal(t, int64(0), timer.intervalsMissed())
+		timer.lastResetTime = now.Add(-15 * time.Second)
+		assert.Equal(t, int64(0), timer.intervalsMissed(now))
 	})
 
 	t.Run("outside second interval", func(t *testing.T) {
 		timer := NewIntervalTimer(15 * time.Second)
-		timer.lastResetTime = time.Now().Add(-31 * time.Second)
-		assert.Equal(t, int64(1), timer.intervalsMissed())
+		timer.lastResetTime = now.Add(-31 * time.Second)
+		assert.Equal(t, int64(1), timer.intervalsMissed(now))
 	})
 
 	t.Run("many intervals late", func(t *testing.T) {
 		timer := NewIntervalTimer(15 * time.Second)
-		timer.lastResetTime = time.Now().Add(-61 * time.Second)
-		assert.Equal(t, int64(3), timer.intervalsMissed())
+		timer.lastResetTime = now.Add(-61 * time.Second)
+		assert.Equal(t, int64(3), timer.intervalsMissed(now))
 	})
 
 	t.Run("near but still before the interval", func(t *testing.T) {
 		timer := NewIntervalTimer(15 * time.Second)
-		timer.lastResetTime = time.Now().Add(-14501 * time.Millisecond)
-		assert.Equal(t, int64(0), timer.intervalsMissed())
+		timer.lastResetTime = now.Add(-14501 * time.Millisecond)
+		assert.Equal(t, int64(0), timer.intervalsMissed(now))
 	})
-
 }
