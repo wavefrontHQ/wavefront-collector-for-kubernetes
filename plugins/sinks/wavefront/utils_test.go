cce063d3ed27c41906f583d360ba1e8b7298a755
Matthew Conger-Eldeen <matthewco@vmware.com>
2021-06-23 15:21:03 -0600

    attempt to reduce excessive taging before we get to the proxy
     * renamed processTags to cleanTags
     * cleanTags always removes all tags starting with label.beta. and label.alpha.
     * cleanTags removes tags with duplicate values iff we have more than the max (20)
     * moved cleanTags from wavefront point converter to wavefront sink
       + not all of the wavefront points were going through the converter
         (some were already converted)
       + tags were being added inside of the wavefront sink
     * added logTagRemoveReason which prints a debug log to detail why we're removing
       tags for a given metric

---
+++ plugins/sinks/wavefront/utils_test.go
@@ -0,0 +1,56 @@
+package wavefront
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCleanTags(t *testing.T) {
+	t.Run("excludes tags in the exclude tag list", func(t *testing.T) {
+		for _, excludedTagName := range excludeTagList {
+			actual := map[string]string{excludedTagName: "some-value"}
+			cleanTags(actual, maxWavefrontTags)
+			assert.Equal(t, map[string]string{}, actual)
+		}
+	})
+
+	t.Run("excludes tags with given prefixes", func(t *testing.T) {
+		for _, excludedTagName := range excludeTagPrefixes {
+			actual := map[string]string{excludedTagName + "/something": "some-value"}
+			cleanTags(actual, maxWavefrontTags)
+			assert.Equal(t, map[string]string{}, actual)
+		}
+	})
+
+	t.Run("excludes empty tags", func(t *testing.T) {
+		actual := map[string]string{"good-tag": ""}
+		cleanTags(actual, maxWavefrontTags)
+		assert.Equal(t, map[string]string{}, actual)
+	})
+
+	t.Run("de-duplicates tag values >= 10 characters when over capacity", func(t *testing.T) {
+		t.Run("when the tag names are different lengths", func(t *testing.T) {
+			actual := map[string]string{"long-tag-name": "some.hostname", "shrt-tg": "some.hostname"}
+			cleanTags(actual, 1)
+			assert.Equal(t, map[string]string{"shrt-tg": "some.hostname"}, actual)
+		})
+
+		t.Run("when the tag names of the same length", func(t *testing.T) {
+			actual := map[string]string{"dup2": "some.hostname", "dup1": "some.hostname"}
+			cleanTags(actual, 1)
+			assert.Equal(t, map[string]string{"dup1": "some.hostname"}, actual)
+		})
+
+		t.Run("when the duplicated values are < 10 characters", func(t *testing.T) {
+			actual := map[string]string{"a-tag": "123", "b-tag": "123"}
+			cleanTags(actual, 1)
+			assert.Equal(t, map[string]string{"a-tag": "123", "b-tag": "123"}, actual)
+		})
+
+		t.Run("when under the max capacity", func(t *testing.T) {
+			actual := map[string]string{"a-tag": "123", "b-tag": "123"}
+			cleanTags(actual, 2)
+			assert.Equal(t, map[string]string{"a-tag": "123", "b-tag": "123"}, actual)
+		})
+	})
+}
