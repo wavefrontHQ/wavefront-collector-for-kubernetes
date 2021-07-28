39e305c6187a1ba6829dcea949fe8271462b16b4
Mark Michael <mamichael@vmware.com>
2021-07-28 11:30:33 -0600

    Adding debuging for travis ci failure

--- hack/kustomize/generate.sh
+++ hack/kustomize/generate.sh
@@ -86,6 +86,10 @@ fi
 
 cat  base/collector.template.yaml overlays/test-$K8S_ENV/collector-config/overrides.yaml > base/collector.yaml
 
+pwd
+ls -l base
+cat  base/collector.yaml
+
 sed -i '' "s/YOUR_CLUSTER_NAME/cluster-${VERSION}/g" base/collector.yaml
 sed -i '' "s/NAMESPACE/${NAMESPACE_NAME}/g" base/collector.yaml
 sed -i '' "s/FLUSH_ONCE/${FLUSH_ONCE}/g" base/collector.yaml
