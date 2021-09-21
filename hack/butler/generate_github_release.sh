e593b60ff65338251770097a8c08b3f6b518c82b
Mark Michael <mamichael@vmware.com>
2021-09-21 09:20:05 -0600

    do github release for real

--- hack/butler/generate_github_release.sh
+++ hack/butler/generate_github_release.sh
@@ -7,7 +7,7 @@ VERSION=$(cat ./release/VERSION)
 GIT_HUB_REPO=wavefrontHQ/wavefront-collector-for-kubernetes
 
 
-echo curl -X POST -H "Content-Type:application/json" \
+curl -X POST -H "Content-Type:application/json" \
 -H "Authorization: token $GITHUB_CREDS_PSW" \
 -d "{
       \"tag_name\": \"v$VERSION\",
