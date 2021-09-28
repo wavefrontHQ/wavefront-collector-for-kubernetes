6b6f86ae989ae8ffb4dfab118b9ea493e8dd97e6
Priyadharshini Selvaganesan <pselvaganesa@vmware.com>
2021-09-28 10:23:40 -0700

    Refactored some names. Created a template for the waitForQueryExists.

--- hack/kustomize/test-e2e.sh
+++ hack/kustomize/test-e2e.sh
@@ -4,8 +4,8 @@ cd "$(dirname $0)"
 source ../deploy/k8s-utils.sh
 
 # This script automates the functional testing of the collector
-
-DEFAULT_VERSION=$(semver-cli inc patch "$(cat ../../release/VERSION)")
+VERSION_FROM_FILE="$(cat ../../release/VERSION)"
+DEFAULT_VERSION=$(semver-cli inc patch $VERSION_FROM_FILE)
 DEFAULT_DOCKER_HOST="wavefronthq"
 
 WAVEFRONT_CLUSTER=$1
@@ -48,7 +48,7 @@ CLUSTER_NAME=$(whoami)-${K8S_ENV}-${VERSION}
 # TODO: generate a unique cluster name or label upon each iteration to ensure entirely new metrics
 # example installation_method="e2e-manual-run-<random-string>"
 
-function waitForQuery() {
+function waitForQueryMatchExact() {
   local query=$1
   local expected=$2
   local actual
@@ -67,13 +67,33 @@ function waitForQuery() {
   done
 }
 
-VERSION_WITHOUT_SECOND_DOT="${DEFAULT_VERSION%.*}"
-VERSION_WITHOUT_SECOND_DOT+="$(echo $DEFAULT_VERSION | cut -d '.' -f3)"
+# TODO: Yet to be verified for implementation
+function waitForQueryExists() {
+  local query=$1
+  local actual
+  while [[ $actual != 0 ]]; do
+    echo "-@-@-@-@-@-BEGIN checking wavefront dashboard stuff for $query-@-@-@-@-@-"
+    actual=$(curl -X GET --header "Accept: application/json" \
+     --header "Authorization: Bearer $API_TOKEN" \
+     "https://$WAVEFRONT_CLUSTER.wavefront.com/api/v2/chart/api?q=${query}&queryType=WQL&s=$AFTER_UNIX_TS&g=s&view=METRIC&sorted=false&cached=true&useRawQK=false" \
+     | jq '.warnings')
+
+    echo "Actual is: '$actual'"
+    echo "-@-@-@-@-@-END checking wavefront dashboard stuff for $query-@-@-@-@-@-"
+
+    sleep $CURL_WAIT
+  done
+}
+
+VERSION_IN_DECIMAL="${VERSION_FROM_FILE%.*}"
+VERSION_IN_DECIMAL+="$(echo ${VERSION_FROM_FILE} | cut -d '.' -f3)"
+PROM_EXAMPLE_EXPECTED_COUNT="3"
 
 # TODO: At this point it is an endless loop of querying. Need to add the actual checking and quitting the loop.
-waitForQuery "ts(kubernetes.collector.version%2C%20cluster%3D%22$CLUSTER_NAME%22%20AND%20installation_method%3D%22manual%22)" "${VERSION_WITHOUT_SECOND_DOT}"
-#waitForQuery "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22$CLUSTER_NAME%22)"
-#waitForQuery "ts(mysql.connections%2C%20cluster%3D%22$CLUSTER_NAME%22)"
-waitForQuery "ts(prom-example.schedule.activity.decision.counter%2C%20cluster%3D%22$CLUSTER_NAME%22)" "3"
+waitForQueryMatchExact "ts(kubernetes.collector.version%2C%20cluster%3D%22$CLUSTER_NAME%22%20AND%20installation_method%3D%22manual%22)" "${VERSION_IN_DECIMAL}"
+#waitForQueryExists "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22$CLUSTER_NAME%22)" # TODO: Yet to be query matched
+#waitForQueryExists "ts(mysql.connections%2C%20cluster%3D%22$CLUSTER_NAME%22)" # TODO: Yet to be query matched
+#waitForQueryExists "ts(mysql.connectionstest%2C%20cluster%3D%22$CLUSTER_NAME%22)" # TODO: Using this to test a failure scenario error message.
+waitForQueryMatchExact "ts(prom-example.schedule.activity.decision.counter%2C%20cluster%3D%22$CLUSTER_NAME%22)" "${PROM_EXAMPLE_EXPECTED_COUNT}"
 
 #exit "$EXIT_CODE" # TODO: Not sure if we need any manipulation with $EXIT_CODE variable here, similar to test-integration.sh
