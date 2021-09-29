075ddaa4d7cfaf2a90558fd01dcbd1fee04a954b
helen-shao <shaoh@vmware.com>
2021-09-29 11:31:59 -0600

    refactor and make target

--- hack/kustomize/test-e2e.sh
+++ hack/kustomize/test-e2e.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-cd "$(dirname $0)"
+cd "$(dirname "$0")"
 source ../deploy/k8s-utils.sh
 
 # This script automates the functional testing of the collector
 VERSION_FROM_FILE="$(cat ../../release/VERSION)"
-DEFAULT_VERSION=$(semver-cli inc patch $VERSION_FROM_FILE)
+DEFAULT_VERSION=$(semver-cli inc patch "$VERSION_FROM_FILE")
 DEFAULT_DOCKER_HOST="wavefronthq"
 
 WAVEFRONT_CLUSTER=$1
@@ -40,58 +40,57 @@ kubectl config set-context --current --namespace=default
 
 echo "deploying collector $IMAGE_NAME $VERSION"
 
-env USE_TEST_PROXY=false ./deploy.sh -c "$WAVEFRONT_CLUSTER" -t "$API_TOKEN" -v $VERSION -d $DOCKER_HOST -k $K8S_ENV
+env USE_TEST_PROXY=false ./deploy.sh -c "$WAVEFRONT_CLUSTER" -t "$API_TOKEN" -v "$VERSION" -d $DOCKER_HOST -k "$K8S_ENV"
 
 wait_for_cluster_ready
 
 AFTER_UNIX_TS="$(date '+%s')000"
 
 CLUSTER_NAME=$(whoami)-${K8S_ENV}-${VERSION}
-# TODO: generate a unique cluster name or label upon each iteration to ensure entirely new metrics
-# example installation_method="e2e-manual-run-<random-string>"
 
-function curlQueryToWFDashboard() {
+function curl_query_to_wf_dashboard() {
+  local query=$1
   actual=$(curl -X GET --header "Accept: application/json" \
    --header "Authorization: Bearer $API_TOKEN" \
     "https://$WAVEFRONT_CLUSTER.wavefront.com/api/v2/chart/api?q=${query}&queryType=WQL&s=$AFTER_UNIX_TS&g=s&view=METRIC&sorted=false&cached=true&useRawQK=false" \
     | jq '.timeseries[0].data[0][1]')
 }
 
-function waitForQueryMatchExact() {
-  local query=$1
+function wait_for_query_match_exact() {
+  local query_match_exact=$1
   local expected=$2
   local actual
   local loop_count=0
-  while [[ $actual != $expected ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; do
+  while [[ $actual != "${expected}" ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; do
     loop_count=$((loop_count+1))
-    echo "===============BEGIN checking wavefront dashboard stuff for $query"
+    echo "===============BEGIN checking wavefront dashboard stuff for $query_match_exact"
     echo "Trying query for $loop_count/$MAX_QUERY_TIMES times"
-    curlQueryToWFDashboard
+    curl_query_to_wf_dashboard "${query_match_exact}"
     echo "Actual is: '$actual'"
-    echo "Expected is '$expected'"
-    echo "===============END checking wavefront dashboard stuff for $query"
+    echo "Expected is '${expected}'"
+    echo "===============END checking wavefront dashboard stuff for $query_match_exact"
 
     sleep $CURL_WAIT
   done
 
-  if [[ $actual != $expected ]] ; then
+  if [[ $actual != "${expected}" ]] ; then
     EXIT_CODE=1
   fi
 }
 
-function waitForQueryNonZero() {
-  local query=$1
+function wait_for_query_non_zero() {
+  local query_non_zero=$1
   local actual=0
   local loop_count=0
   while [[ $actual == null || $actual == 0 ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; do
     loop_count=$((loop_count+1))
 
-    echo "===============BEGIN checking wavefront dashboard stuff for $query"
+    echo "===============BEGIN checking wavefront dashboard stuff for $query_non_zero"
     echo "Trying query for $loop_count/$MAX_QUERY_TIMES times"
-    curlQueryToWFDashboard
+    curl_query_to_wf_dashboard "${query_non_zero}"
     echo "Actual is: '$actual'"
     echo "Expected non zero"
-    echo "===============END checking wavefront dashboard stuff for $query"
+    echo "===============END checking wavefront dashboard stuff for $query_non_zero"
 
     sleep $CURL_WAIT
   done
@@ -102,14 +101,13 @@ function waitForQueryNonZero() {
 }
 
 VERSION_IN_DECIMAL="${VERSION_FROM_FILE%.*}"
-VERSION_IN_DECIMAL+="$(echo ${VERSION_FROM_FILE} | cut -d '.' -f3)"
+VERSION_IN_DECIMAL+="$(echo "${VERSION_FROM_FILE}" | cut -d '.' -f3)"
 PROM_EXAMPLE_EXPECTED_COUNT="3"
 
-# TODO: make queries parallel?
-waitForQueryMatchExact "ts(kubernetes.collector.version%2C%20cluster%3D%22$CLUSTER_NAME%22%20AND%20installation_method%3D%22manual%22)" "${VERSION_IN_DECIMAL}"
-waitForQueryNonZero "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22$CLUSTER_NAME%22)"
-waitForQueryNonZero "ts(mysql.connections%2C%20cluster%3D%22$CLUSTER_NAME%22)"
-waitForQueryMatchExact "ts(prom-example.schedule.activity.decision.counter%2C%20cluster%3D%22$CLUSTER_NAME%22)" "${PROM_EXAMPLE_EXPECTED_COUNT}"
+wait_for_query_match_exact "ts(kubernetes.collector.version%2C%20cluster%3D%22${CLUSTER_NAME}%22%20AND%20installation_method%3D%22manual%22)" "${VERSION_IN_DECIMAL}"
+wait_for_query_non_zero "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22${CLUSTER_NAME}%22)"
+wait_for_query_non_zero "ts(mysql.connections%2C%20cluster%3D%22${CLUSTER_NAME}%22)"
+wait_for_query_match_exact "ts(prom-example.schedule.activity.decision.counter%2C%20cluster%3D%22${CLUSTER_NAME}%22)" "${PROM_EXAMPLE_EXPECTED_COUNT}"
 
 echo "Exit with code '$EXIT_CODE'"
 exit "$EXIT_CODE"
