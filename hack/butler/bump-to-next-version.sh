5e643b7a4dde8e37a74cfdfbf5a28f7811d416a0
Priya Selvaganesan <pselvaganesa@vmware.com>
2021-10-06 14:34:47 -0600

    revert version back to 1.7.1 and clean up unnecessary code
    Signed-off-by: John Cornish <jcornish@vmware.com>

--- hack/butler/bump-to-next-version.sh
+++ hack/butler/bump-to-next-version.sh
@@ -5,14 +5,11 @@ echo ${OLD_VERSION}
 ## Bump to next version
 
 sed -i "s/${OLD_VERSION}/${NEXT_VERSION}/g" "deploy/kubernetes/5-collector-daemonset.yaml"
-sed -i "s/${OLD_VERSION}/${NEXT_VERSION}/g" "deploy/openshift/collector/3-collector-deployment.yaml"
 echo "$NEXT_VERSION" >./VERSION
 
 git commit -am "bump version to ${NEXT_VERSION}"
 
 git push --set-upstream origin "$GIT_BRANCH"
-
-#gh pr create --base master --fill --head "$GIT_BRANCH" --web
 
 curl \
   -X POST \
