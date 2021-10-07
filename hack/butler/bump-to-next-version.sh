a89d76023c70b6ef26e19e5b15eac207555fda23
John Cornish <jcornish@vmware.com>
2021-10-07 10:27:56 -0700

    Put the version in the existing correct VERSION file.
    Signed-off-by: Priya Selvaganesan <pselvaganesa@vmware.com>

--- hack/butler/bump-to-next-version.sh
+++ hack/butler/bump-to-next-version.sh
@@ -5,8 +5,9 @@ echo ${OLD_VERSION}
 ## Bump to next version
 pwd
 sed -i "s/${OLD_VERSION}/${NEXT_VERSION}/g" "deploy/kubernetes/5-collector-daemonset.yaml"
-echo "$NEXT_VERSION" >./VERSION
-cat ./VERSION
+echo "$NEXT_VERSION" >./release/VERSION
+cat ./release/VERSION
+
 git commit -am "bump version to ${NEXT_VERSION}"
 
 git push --set-upstream origin "$GIT_BRANCH"
