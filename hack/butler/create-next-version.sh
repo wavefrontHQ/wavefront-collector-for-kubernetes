b111f2a4ea334138cb544edba7ce8ebe8589887f
Priya Selvaganesan <pselvaganesa@vmware.com>
2021-10-21 16:08:19 -0600

    WIP: refactor bump and PR scripts
    Signed-off-by: Mark Michael <mamichael@vmware.com>

--- hack/butler/create-next-version.sh
+++ hack/butler/create-next-version.sh
@@ -16,10 +16,14 @@ fi
 
 
 OLD_VERSION=$(cat ../../release/VERSION)
-echo "${OLD_VERSION}" > ./OLD_VERSION
-NEXT_VERSION=$(semver-cli inc "$BUMP_COMPONENT" "$OLD_VERSION")
-echo "${NEXT_VERSION}" > ./NEXT_VERSION
+NEW_VERSION=$(semver-cli inc "$BUMP_COMPONENT" "$OLD_VERSION")
+echo "$NEW_VERSION" >../../release/VERSION
 
-GIT_BRANCH="bump-${NEXT_VERSION}"
-git checkout -b "$GIT_BRANCH"
-echo "${GIT_BRANCH}" > ./GIT_BUMP_BRANCH_NAME
+GIT_BUMP_BRANCH_NAME="bump-${NEW_VERSION}"
+git checkout -b "$GIT_BUMP_BRANCH_NAME"
+
+echo "Bumping ${OLD_VERSION} to ${NEW_VERSION} in ../../deploy/kubernetes/5-collector-daemonset.yaml"
+sed -i '' "s/${OLD_VERSION}/${NEW_VERSION}/g" "../../deploy/kubernetes/5-collector-daemonset.yaml"
+git commit -am "bump version to ${NEW_VERSION}"
+
+git push --set-upstream origin "${GIT_BUMP_BRANCH_NAME}"
--- hack/butler/merge-pull-request.sh
+++ hack/butler/merge-pull-request.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 set -ex
 
-cd "$(dirname "$0")" # cd to directory that bump-version-and-raise-pull-request.sh is in
+cd "$(dirname "$0")" # cd to directory that merge-pull-request.sh is in
 
-echo "Bumping ${OLD_VERSION} to ${NEXT_VERSION} in ../../deploy/kubernetes/5-collector-daemonset.yaml"
-sed -i "s/${OLD_VERSION}/${NEXT_VERSION}/g" "../../deploy/kubernetes/5-collector-daemonset.yaml"
-echo "$NEXT_VERSION" >../../release/VERSION
+VERSION=$(cat ../../release/VERSION)
+GIT_BUMP_BRANCH_NAME="bump-${VERSION}"
 
-git commit -am "bump version to ${NEXT_VERSION}"
-
-git push --set-upstream origin "${GIT_BUMP_BRANCH_NAME}"
-
-curl \
+PR_URL=$(curl \
   -X POST \
   -H "Authorization: token ${TOKEN}" \
-  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"master\",\"title\":\"Bump version to ${NEXT_VERSION}\"}" \
-  https://api.github.com/repos/wavefrontHQ/wavefront-collector-for-kubernetes/pulls
+  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"master\",\"title\":\"Bump version to ${VERSION}\"}" \
+  https://api.github.com/repos/wavefrontHQ/wavefront-collector-for-kubernetes/pulls |
+  jq -r '.url')
 
+echo "PR URL: ${PR_URL}"
 
 curl \
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/octocat/hello-world/pulls/42/merge \
-  -d '{"commit_title":"commit_title"}'
+  "${PR_URL}/merge" \
+  -d "{\"commit_title\":\"Bump version to ${VERSION}\"}"
