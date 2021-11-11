7024f4872cbc44a56a1296a9ed2297636ff2ce1a
helen-shao <shaoh@vmware.com>
2021-11-11 15:32:23 -0700

    WIP: change test-proxy, filter invalid char, change wait time

--- cmd/test-proxy/metric.go
+++ cmd/test-proxy/metric.go
@@ -1,5 +1,7 @@
 package main
 
+import "regexp"
+
 type Metric struct {
 	Name      string
 	Value     string
@@ -8,6 +10,13 @@ type Metric struct {
 }
 
 func ParseMetric(line string) (*Metric, error) {
+	// To remove the special character ∆ from the metrics ∆~sdk.go.core.*
+	reg, err := regexp.Compile("[∆]+")
+	if err != nil {
+		return nil, err
+	}
+	line = reg.ReplaceAllString(line, "")
+
 	g := &MetricGrammar{Buffer: line}
 	g.Init()
 	if err := g.Parse(); err != nil {
