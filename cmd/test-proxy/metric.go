6a45125dde907d32fc4b548180ebb5dc47a9ccae
Matthew Conger-Eldeen <matthewco@vmware.com>
2021-11-15 11:17:14 -0700

    make the test-proxy parse delta metrics correctly

--- cmd/test-proxy/metric.go
+++ cmd/test-proxy/metric.go
@@ -1,7 +1,5 @@
 package main
 
-import "regexp"
-
 type Metric struct {
 	Name      string
 	Value     string
@@ -10,13 +8,6 @@ type Metric struct {
 }
 
 func ParseMetric(line string) (*Metric, error) {
-	// To remove the special character ∆ from the metrics ∆~sdk.go.core.*
-	reg, err := regexp.Compile("[∆]+")
-	if err != nil {
-		return nil, err
-	}
-	line = reg.ReplaceAllString(line, "")
-
 	g := &MetricGrammar{Buffer: line}
 	g.Init()
 	if err := g.Parse(); err != nil {
