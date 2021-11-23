20590b1956ebaf13480cef7c62f1d8a2dbc98da8
John Cornish <jcornish@vmware.com>
2021-11-23 16:52:23 -0700

    move ./release/deploy-local* scripts to ./hack/test/deploy
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- hack/test/deploy/deploy-local-alpha.sh
+++ hack/test/deploy/deploy-local-alpha.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")" # cd to deploy-local-alpha.sh is in
 
-source "../hack/test/deploy/k8s-utils.sh"
+source "./k8s-utils.sh"
 
 if [[ -z ${CURRENT_VERSION} ]] ; then
     print_msg_and_exit "Need to specify alpha version image tag by setting CURRENT_VERSION"
--- hack/test/deploy/deploy-local.sh
+++ hack/test/deploy/deploy-local.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")" # cd to deploy-local.sh is in
 
-source "../hack/test/deploy/k8s-utils.sh"
+source "./k8s-utils.sh"
 
 if [[ -z ${WAVEFRONT_TOKEN} ]] ; then
     print_msg_and_exit "WAVEFRONT_TOKEN required"
@@ -17,7 +17,7 @@ CURRENT_VERSION=${CURRENT_VERSION:-$VERSION}
 COLLECTOR_REPO=projects.registry.vmware.com/tanzu_observability/kubernetes-collector
 CURRENT_COLLECTOR_REPO=${CURRENT_COLLECTOR_REPO:-$COLLECTOR_REPO}
 
-pushd ../
+pushd "$ROOT_DIR"
   make clean-deployment
   make deploy-targets
 popd
@@ -41,12 +41,12 @@ cp "$ROOT_DIR/hack/test/deploy/mysql-config.yaml" "$TEMP_DIR/."
 cp "$ROOT_DIR/hack/test/deploy/prom-example.yaml" "$TEMP_DIR/."
 
 pushd "$TEMP_DIR"
-  sed -i "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" "$TEMP_DIR/6-wavefront-proxy.yaml"
-  sed -i "s/k8s-cluster/${CONFIG_CLUSTER_NAME}/g" "$TEMP_DIR/4-collector-config.yaml"
+  sed -i '' "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" "$TEMP_DIR/6-wavefront-proxy.yaml"
+  sed -i '' "s/k8s-cluster/${CONFIG_CLUSTER_NAME}/g" "$TEMP_DIR/4-collector-config.yaml"
 
   echo "using version ${CURRENT_VERSION}"
-  sed -i "s/$VERSION/$CURRENT_VERSION/g" "$TEMP_DIR/5-collector-daemonset.yaml"
-  sed -i "s%${COLLECTOR_REPO}%${CURRENT_COLLECTOR_REPO}%g" "$TEMP_DIR/5-collector-daemonset.yaml"
+  sed -i '' "s/$VERSION/$CURRENT_VERSION/g" "$TEMP_DIR/5-collector-daemonset.yaml"
+  sed -i '' "s%${COLLECTOR_REPO}%${CURRENT_COLLECTOR_REPO}%g" "$TEMP_DIR/5-collector-daemonset.yaml"
 
   kubectl config set-context --current --namespace="$NS"
   kubectl apply -f "$TEMP_DIR/."
