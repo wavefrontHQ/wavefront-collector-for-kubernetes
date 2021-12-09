086134007e21c79dca72d24378bc47b0ee8845f8
John Cornish <jcornish@vmware.com>
2021-12-09 14:10:52 -0700

    Extend deploy-local.sh to differentiate between collector and proxy
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- hack/test/deploy/deploy-local-alpha.sh
+++ hack/test/deploy/deploy-local-alpha.sh
@@ -5,8 +5,8 @@ cd "$(dirname "$0")" # cd to deploy-local-alpha.sh is in
 
 source "./k8s-utils.sh"
 
-if [[ -z ${CURRENT_VERSION} ]] ; then
-    print_msg_and_exit "Need to specify alpha version image tag by setting CURRENT_VERSION"
+if [[ -z ${CURRENT_COLLECTOR_VERSION} ]] ; then
+    print_msg_and_exit "Need to specify alpha version image tag by setting CURRENT_COLLECTOR_VERSION"
 fi
 
 CURRENT_COLLECTOR_REPO=projects.registry.vmware.com/tanzu_observability_keights_saas/kubernetes-collector-snapshot \
