2a61e7eba87022dbb4d48743b5d36c6089732998
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-02-10 11:51:21 -0700

    Automate using a deploy script
    Signed-off-by: Mark Michael <mamichael@vmware.com>

---
+++ hack/test/dashboards/deploy/deploy-to-demo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+sudo su -
+cd /home/k8po/wavefront-collector-for-kubernetes/hack/test/dashboards/deploy/
+sed -i "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" "6-wavefront-proxy.yaml"
+
+kubectl delete -f . || true
+kubectl apply -f .
