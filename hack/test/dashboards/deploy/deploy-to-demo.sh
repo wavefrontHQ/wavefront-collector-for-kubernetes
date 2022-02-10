ce6425685ec55767f993718ee7a7367177eadc65
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-02-10 12:02:52 -0700

    Remove sudo since it quits the process
    Signed-off-by: Mark Michael <mamichael@vmware.com>

--- hack/test/dashboards/deploy/deploy-to-demo.sh
+++ hack/test/dashboards/deploy/deploy-to-demo.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo su -
 cd /home/k8po/wavefront-collector-for-kubernetes/hack/test/dashboards/deploy/
 sed -i "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" "6-wavefront-proxy.yaml"
 
