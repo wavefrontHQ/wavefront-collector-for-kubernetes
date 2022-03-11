1ca530744347ee7865b153bc712711a79806226b
Mark Michael <mamichael@vmware.com>
2022-03-11 14:46:21 -0700

    change the mysql and memached persistent volume size from 8Gi to 200Mi

--- hack/test/deploy/deploy-targets.sh
+++ hack/test/deploy/deploy-targets.sh
@@ -23,10 +23,12 @@ kubectl apply -f jobs.yaml &>/dev/null || true
 helm repo add bitnami https://charts.bitnami.com/bitnami &>/dev/null || true
 helm upgrade --install memcached-release bitnami/memcached \
 --set resources.requests.memory="100Mi",resources.requests.cpu="100m" \
+--set persistence.size=200Mi \
 --namespace collector-targets &>/dev/null || true
 
 helm upgrade --install mysql-release bitnami/mysql \
 --set auth.rootPassword=password123 \
+--set primary.persistence.size=200Mi \
 --namespace collector-targets &>/dev/null || true
 
 wait_for_cluster_ready
