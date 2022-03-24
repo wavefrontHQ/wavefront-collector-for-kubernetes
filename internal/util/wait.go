128172bab2ae49a123a7abb57c902f7d21b289b6
John Cornish <jcornish@vmware.com>
2022-03-24 14:46:27 -0700

    Ported runtime logic over to wait.go
    Signed-off-by: Priya Selvaganesan <pselvaganesa@vmware.com>

--- internal/util/wait.go
+++ internal/util/wait.go
@@ -1,13 +1,24 @@
 package util
 
 import (
+	"k8s.io/klog/v2"
+	"net/http"
+	"runtime"
 	"time"
-
-	"k8s.io/apimachinery/pkg/util/runtime"
 )
 
-// NeverStop may be passed to make it never stop
-var NeverStop <-chan struct{} = make(chan struct{})
+var (
+	// ReallyCrash controls the behavior of HandleCrash and now defaults
+	// true. It's still exposed so components can optionally set to false
+	// to restore prior behavior.
+	ReallyCrash = true
+
+	// NeverStop may be passed to make it never stop
+	NeverStop <-chan struct{} = make(chan struct{})
+
+	// PanicHandlers is a list of functions which will be invoked when a panic happens.
+	PanicHandlers = []func(interface{}){logPanic}
+)
 
 // Retry makes the function run infinitely after certain time period
 func Retry(f func(), duration time.Duration, stopCh <-chan struct{}) {
@@ -23,8 +34,45 @@ func Retry(f func(), duration time.Duration, stopCh <-chan struct{}) {
 		}
 
 		func() {
-			defer runtime.HandleCrash()
+			defer HandleCrash()
 			f()
 		}()
+	}
+}
+
+func HandleCrash(additionalHandlers ...func(interface{})) {
+	if r := recover(); r != nil {
+		for _, fn := range PanicHandlers {
+			fn(r)
+		}
+		for _, fn := range additionalHandlers {
+			fn(r)
+		}
+		if ReallyCrash {
+			// Actually proceed to panic.
+			panic(r)
+		}
+	}
+}
+
+// logPanic logs the caller tree when a panic occurs (except in the special case of http.ErrAbortHandler).
+func logPanic(r interface{}) {
+	if r == http.ErrAbortHandler {
+		// honor the http.ErrAbortHandler sentinel panic value:
+		//   ErrAbortHandler is a sentinel panic value to abort a handler.
+		//   While any panic from ServeHTTP aborts the response to the client,
+		//   panicking with ErrAbortHandler also suppresses logging of a stack trace to the server's error log.
+		return
+	}
+
+	// Same as stdlib http server code. Manually allocate stack trace buffer size
+	// to prevent excessively large logs
+	const size = 64 << 10
+	stacktrace := make([]byte, size)
+	stacktrace = stacktrace[:runtime.Stack(stacktrace, false)]
+	if _, ok := r.(string); ok {
+		klog.Errorf("Observed a panic: %s\n%s", r, stacktrace)
+	} else {
+		klog.Errorf("Observed a panic: %#v (%v)\n%s", r, r, stacktrace)
 	}
 }
