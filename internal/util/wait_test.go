4825377713056e8af6dfa12f1f15b0ebf3d7c766
Mark Michael <mamichael@vmware.com>
2022-03-25 15:50:34 -0600

    Adds tests for the util wait.Retry class.  Removes initial wait in watcher.go since the Retry functions waits before first run

---
+++ internal/util/wait_test.go
@@ -0,0 +1,33 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//+build !race
+
+package util
+import (
+    "testing"
+    "time"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestRetry(t *testing.T) {
+    counter := 0
+    retryFunc := func() {counter++}
+
+    t.Run("Test stop", func(t *testing.T) {
+        stopCh := make(chan struct{})
+        go Retry(retryFunc, 1*time.Second, stopCh)
+        time.Sleep(1500*time.Millisecond)
+        close(stopCh)
+        assert.Equal(t, 1, counter)
+    })
+
+    t.Run("Test delay before first retry", func(t *testing.T) {
+        counter = 0
+        stopCh := make(chan struct{})
+        go Retry(retryFunc, 1*time.Second, stopCh)
+        time.Sleep(500*time.Millisecond)
+        close(stopCh)
+        assert.Equal(t, 0, counter)
+    })
+}
