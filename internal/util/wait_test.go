af351d67440d20fa51fc4af5394a900d3085f9cc
Mark Michael <mamichael@vmware.com>
2022-03-28 10:47:35 -0600

    fmt
    Signed-off-by: John Cornish <jcornish@vmware.com>

--- internal/util/wait_test.go
+++ internal/util/wait_test.go
@@ -3,31 +3,32 @@
 //+build !race
 
 package util
-import (
-    "testing"
-    "time"
 
-    "github.com/stretchr/testify/assert"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRetry(t *testing.T) {
-    counter := 0
-    retryFunc := func() {counter++}
+	counter := 0
+	retryFunc := func() { counter++ }
 
-    t.Run("Test stop", func(t *testing.T) {
-        stopCh := make(chan struct{})
-        go Retry(retryFunc, 1*time.Second, stopCh)
-        time.Sleep(1500*time.Millisecond)
-        close(stopCh)
-        assert.Equal(t, 1, counter)
-    })
+	t.Run("Test stop", func(t *testing.T) {
+		stopCh := make(chan struct{})
+		go Retry(retryFunc, 1*time.Second, stopCh)
+		time.Sleep(1500 * time.Millisecond)
+		close(stopCh)
+		assert.Equal(t, 1, counter)
+	})
 
-    t.Run("Test delay before first retry", func(t *testing.T) {
-        counter = 0
-        stopCh := make(chan struct{})
-        go Retry(retryFunc, 1*time.Second, stopCh)
-        time.Sleep(500*time.Millisecond)
-        close(stopCh)
-        assert.Equal(t, 0, counter)
-    })
+	t.Run("Test delay before first retry", func(t *testing.T) {
+		counter = 0
+		stopCh := make(chan struct{})
+		go Retry(retryFunc, 1*time.Second, stopCh)
+		time.Sleep(500 * time.Millisecond)
+		close(stopCh)
+		assert.Equal(t, 0, counter)
+	})
 }
