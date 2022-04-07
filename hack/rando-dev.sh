b95eec4d380b123676cc7f6f3641e6b135c48dbe
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-04-07 13:01:41 -0700

    Quotes needed

--- hack/rando-dev.sh
+++ hack/rando-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-curl "https://www.random.org/lists/?mode=advanced" \
-  -H "authority: www.random.org" \
+curl 'https://www.random.org/lists/?mode=advanced' \
+  -H 'authority: www.random.org' \
   -H 'content-type: application/x-www-form-urlencoded' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
   -H 'sec-fetch-site: same-origin' \
@@ -9,5 +9,5 @@ curl "https://www.random.org/lists/?mode=advanced" \
   -H 'sec-fetch-user: ?1' \
   -H 'sec-fetch-dest: document' \
   -H 'referer: https://www.random.org/lists/?mode=advanced' \
-  --data "list=Anil%0D%0AMark%0D%0APriya%0D%0AMatt%0D%0AJohn%0D%0AHelen%0D%0AJoe&format=plain&rnd=new" \
+  --data 'list=Anil%0D%0AMark%0D%0APriya%0D%0AMatt%0D%0AJohn%0D%0AHelen%0D%0AJoe&format=plain&rnd=new' \
   --compressed
