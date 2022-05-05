cfcf9298969e45ca629cb448ea881e3d69f8f1e7
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-05-05 10:50:57 -0600

    Downgrade kubectl to v1.23.6 due to compatiblity issues
    the aws-cli produces a kubeconfig that is incompatible with v1.24.x version of kubectl.
    see https://github.com/aws/aws-cli/issues/6920 for more details

--- hack/jenkins/setup-for-integration-test.sh
+++ hack/jenkins/setup-for-integration-test.sh
@@ -38,7 +38,8 @@ docker-credential-gcr configure-docker --registries="us.gcr.io"
 #
 # kubectl
 #
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+#curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
