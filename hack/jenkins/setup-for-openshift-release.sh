520991a8dfd2aa0c9f98053e69e4259a23eeef87
Mark Michael <mamichael@vmware.com>
2022-05-05 15:17:26 -0600

    WIP:preflight 2

--- hack/jenkins/setup-for-openshift-release.sh
+++ hack/jenkins/setup-for-openshift-release.sh
@@ -5,6 +5,6 @@
 #
 if ! [ -x "$(command -v preflight)" ]; then
     curl -LO https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/1.1.1/preflight-linux-amd64
-    chmod +x ./preflight
-    sudo mv ./preflight /usr/local/bin/preflight
+    chmod +x ./preflight-linux-amd64
+    sudo mv ./preflight-linux-amd64 /usr/local/bin/preflight
 fi
