c36fe47ad0f29109793ab99f78b7b52e5613897e
Anil Kodali <akodali@vmware.com>
2022-05-10 13:29:46 -0700

    refactor branch name parameter, retrieve release version from branch.

--- hack/jenkins/release-openshift-container.sh
+++ hack/jenkins/release-openshift-container.sh
@@ -5,9 +5,8 @@ REDHAT_CREDS_USR=$2
 REDHAT_CREDS_PSW=$3
 REDHAT_API_KEY=$4
 REDHAT_PROJECT_ID=$5
-GIT_BRANCH=$6
-VERSION=$7
-RC_NUMBER=$8
+GIT_BUMP_BRANCH_NAME=$6
+RC_NUMBER=$7
 
 if ! [ -x "$(command -v preflight)" ]; then
     curl -LO https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/1.1.1/preflight-linux-amd64
@@ -20,8 +19,8 @@ git clean -dfx
 git checkout -- .
 git checkout main
 git pull
-git checkout ${GIT_BRANCH}
-
+git checkout ${GIT_BUMP_BRANCH_NAME}
+VERSION=$(cat release/VERSION)
 TAG_VERSION=${VERSION}-rc${RC_NUMBER}
 
 podman login ${PREFIX} -u ${REDHAT_CREDS_USR} -p ${REDHAT_CREDS_PSW}
@@ -33,4 +32,4 @@ preflight check container ${PREFIX}/wavefront:${TAG_VERSION} --pyxis-api-token=$
 
 # At wrap up, delete local bump branch to keep Openshift VM clean
 git checkout main
-git branch -D ${GIT_BRANCH}
+git branch -D ${GIT_BUMP_BRANCH_NAME}
