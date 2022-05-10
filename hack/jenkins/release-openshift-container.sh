722728daa1ea4071f00d3cac017e5c05d6856a28
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-05-10 12:28:16 -0700

    Complete tested

--- hack/jenkins/release-openshift-container.sh
+++ hack/jenkins/release-openshift-container.sh
@@ -9,9 +9,6 @@ GIT_BRANCH=$6
 VERSION=$7
 RC_NUMBER=$8
 
-#
-# preflight
-#
 if ! [ -x "$(command -v preflight)" ]; then
     curl -LO https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/1.1.1/preflight-linux-amd64
     chmod +x ./preflight-linux-amd64
