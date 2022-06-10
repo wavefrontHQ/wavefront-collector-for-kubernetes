ef858dffad76e15a3cdc9cabd5c43c481fdb7968
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-06-10 14:18:04 -0600

    make it fail again

--- hack/diff_dependencies.sh
+++ hack/diff_dependencies.sh
@@ -5,7 +5,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 function main() {
     cd "$REPO_ROOT"
 
-    GOOS=linux go list -mod=readonly -deps -f '{{ if and (.DepOnly) (.Module) (not .Standard) }}{{ $mod := (or .Module.Replace .Module) }}{{ $mod.Path }}{{ end }}' ./... | grep -v github.com/wavefronthq/wavefront-collector-for-kubernetes | sort -u > /tmp/from_go_mod.txt
+    GOOS=darwin go list -mod=readonly -deps -f '{{ if and (.DepOnly) (.Module) (not .Standard) }}{{ $mod := (or .Module.Replace .Module) }}{{ $mod.Path }}{{ end }}' ./... | grep -v github.com/wavefronthq/wavefront-collector-for-kubernetes | sort -u > /tmp/from_go_mod.txt
     grep '   >>> ' open_source_licenses.txt | grep -v Apache | grep -v Mozilla | awk '{print $2}' | rev | awk -F'v-' '{print $2}' | rev | sort -u > /tmp/from_open_source_licenses.txt
 
     diff -u /tmp/from_go_mod.txt /tmp/from_open_source_licenses.txt
