fd6b7513ede80752fc55e3101f69fb1f000da2b9
Anil Kodali <akodali@vmware.com>
2022-07-21 11:35:35 -0700

    WIP: Modify source as per tag priority.

--- plugins/sinks/wavefront/sink_test.go
+++ plugins/sinks/wavefront/sink_test.go
@@ -121,6 +121,32 @@ func TestCleansTagsBeforeSending(t *testing.T) {
 	assert.NotContains(t, getMetrics(sink), "emptyTag")
 }
 
+func TestClusterSource(t *testing.T) {
+    cfg := configuration.WavefrontSinkConfig{
+        ProxyAddress: "wavefront-proxy:2878",
+        TestMode:     true,
+        Transforms: configuration.Transforms{
+            Prefix: "test.",
+        },
+        ClusterName: "fakeCluster",
+    }
+    sink, err := NewWavefrontSink(cfg)
+    assert.NoError(t, err)
+
+    db := metrics.Batch{
+        Points: []*wf.Point{
+            wf.NewPoint("metric.source.cluster", 1.0, 0, "fakeSource", map[string]string{}),
+            wf.NewPoint("metric.source.cluster.ns", 1.0, 0, "fakeSource", map[string]string{"namespace_name":"fakeNamespace", "nodename":"fakeNodeName"}),
+            wf.NewPoint("metric.source.cluster.node", 1.0, 0, "fakeSource", map[string]string{"nodename":"fakeNodeName"}),
+        },
+    }
+    sink.Export(&db)
+    t.Log(getMetrics(sink))
+    //assert.True(t, strings.Contains(getMetrics(sink), "\"source=fakeCluster\"))
+    //assert.True(t, strings.Contains(getMetrics(sink), "\"source=fakeCluster.fakeNamespace\"))
+    //assert.True(t, strings.Contains(getMetrics(sink), "\"source=fakeCluster.fakeNodeName\"))
+}
+
 func getMetrics(sink WavefrontSink) string {
 	return strings.TrimSpace(sink.(*wavefrontSink).WavefrontClient.(*TestSender).GetReceivedLines())
 }
