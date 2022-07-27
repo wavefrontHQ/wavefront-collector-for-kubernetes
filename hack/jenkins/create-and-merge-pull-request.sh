13b5fd45a919de1c40695e3f9ee78779712da09c
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-07-27 15:31:29 -0600

    change base branch from master to main for release pipeline
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- hack/jenkins/create-and-merge-pull-request.sh
+++ hack/jenkins/create-and-merge-pull-request.sh
@@ -9,7 +9,7 @@ GIT_BUMP_BRANCH_NAME="bump-${VERSION}"
 PR_URL=$(curl \
   -X POST \
   -H "Authorization: token ${TOKEN}" \
-  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"master\",\"title\":\"Bump version to ${VERSION}\"}" \
+  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"main\",\"title\":\"Bump version to ${VERSION}\"}" \
   https://api.github.com/repos/wavefrontHQ/wavefront-collector-for-kubernetes/pulls |
   jq -r '.url')
 
