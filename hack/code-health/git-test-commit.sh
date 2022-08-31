d8fb66fcd35addbb16c78d94a49baa4adb566797
John Cornish <jcornish@vmware.com>
2022-08-31 16:22:16 -0600

    try adding total runtime to commit message
        - total test runtime: 159

--- hack/code-health/git-test-commit.sh
+++ hack/code-health/git-test-commit.sh
@@ -5,13 +5,21 @@ function gtc() {
     # TODO automatically launch kind if not running
     local kind_clusters=$(kind get clusters 2>&1 1>/dev/null)
 
+    local start_time=`date +%s`
+
     if [ "${kind_clusters}" = "No kind clusters found." ]; then
         make nuke-kind
     fi
+
     make tests || return 1
     make integration-test || return 1
 
+    local end_time=`date +%s`
+
+    local total_runtime=$((end_time-start_time))
     git status
-    git commit -m "${message}"
+    git commit -m "${message}
+
+    - total test runtime: ${total_runtime}"
     git push
 }
