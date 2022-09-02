14aecb3bbc45e8e9293d32c60cb81281193689d8
John Cornish <jcornish@vmware.com>
2022-09-02 16:15:52 -0600

    add code health and support stuff

---
+++ hack/code-health-and-support/git-test-commit.sh
@@ -0,0 +1,25 @@
+# NOTE this file should be sourced
+
+function gtc() {
+    local message=$1
+    # TODO automatically launch kind if not running
+    local kind_clusters=$(kind get clusters 2>&1 1>/dev/null)
+
+    local start_time=`date +%s`
+
+    if [ "${kind_clusters}" = "No kind clusters found." ]; then
+        make nuke-kind
+    fi
+
+    make tests || return 1
+    make integration-test || return 1
+
+    local end_time=`date +%s`
+
+    local total_runtime=$((end_time-start_time))
+    git status
+    git commit -m "${message}
+
+    - total test runtime: ${total_runtime}s"
+    git push
+}
