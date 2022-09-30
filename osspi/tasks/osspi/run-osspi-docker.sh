ffa89a03d9671591f736711ebc7e177925091b4c
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-09-30 15:28:02 -0700

    Refactor names

--- osspi/tasks/osspi/run-osspi-docker.sh
+++ osspi/tasks/osspi/run-osspi-docker.sh
@@ -43,23 +43,22 @@ else
 fi
 echo "MASTER_PACKAGE_ID: '${MASTER_PACKAGE_ID}'"
 
-echo "Getting CT_TRACKER_ID from osm release ID that belongs to MASTER_PACKAGE_ID ${MASTER_PACKAGE_ID} if exists. If not, create it and return the CT_TRACKER_ID."
-DISTRIBUTED_CALLING_EXISTING_CLASSES=1
+echo "Getting CT_TRACKER_ID from osm release ID that belongs to MASTER_PACKAGE_ID ${MASTER_PACKAGE_ID} if it exists. If not, create it and return the CT_TRACKER_ID."
 CT_TRACKER_URL="$ENDPOINT/api/public/v1/package/?release=$RELEASE_ID&master_package=$MASTER_PACKAGE_ID"
 echo "CT_TRACKER_URL: '${CT_TRACKER_URL}'"
-CT_TRACKER_REQUEST=$(curl -H "Authorization: ApiKey $USERNAME:$API_KEY" "$CT_TRACKER_URL")
-if [ $(echo "$CT_TRACKER_REQUEST" | jq .count) == 1 ]; then
+CT_TRACKER_LIST_REQUEST=$(curl -H "Authorization: ApiKey $USERNAME:$API_KEY" "$CT_TRACKER_URL")
+if [ $(echo "CT_TRACKER_LIST_REQUEST" | jq .count) == 1 ]; then
   echo "ct-tracker-${CT_TRACKER_OS} found in ${PRODUCT} release"
-  CT_TRACKER_ID=$(echo "$CT_TRACKER_REQUEST" | jq ".results[].id")
+  CT_TRACKER_ID=$(echo "CT_TRACKER_LIST_REQUEST" | jq ".results[].id")
 else
   echo "CT_TRACKER_ID not found in osm release ID. Creating a new CT_TRACKER_ID."
-  CT_TRACKER_REQUEST=$(curl --request POST -H "Authorization: ApiKey $USERNAME:$API_KEY" -H "Content-Type: application/json" --data "{\"release_id\":\"$RELEASE_ID\",\"master_package_id\":\"$MASTER_PACKAGE_ID\",\"interaction_type_id\":[\"1\"],\"modified\":\"No\"}" "$ENDPOINT/api/public/v1/package/")
-  echo "CT_TRACKER_REQUEST results: '${CT_TRACKER_REQUEST}'"
-  if [ $(echo "$CT_TRACKER_REQUEST" | jq -r ".err_code") == 40904  ]; then
-    ERROR_MESSAGE=$(echo "$CT_TRACKER_REQUEST" | jq -r ".err_msg")
+  CT_TRACKER_CREATE_REQUEST=$(curl --request POST -H "Authorization: ApiKey $USERNAME:$API_KEY" -H "Content-Type: application/json" --data "{\"release_id\":\"$RELEASE_ID\",\"master_package_id\":\"$MASTER_PACKAGE_ID\",\"interaction_type_id\":[\"1\"],\"modified\":\"No\"}" "$ENDPOINT/api/public/v1/package/")
+  echo "CT_TRACKER_REQUEST results: '${CT_TRACKER_CREATE_REQUEST}'"
+  if [ $(echo "CT_TRACKER_CREATE_REQUEST" | jq -r ".err_code") == 40904  ]; then
+    ERROR_MESSAGE=$(echo "$CT_TRACKER_CREATE_REQUEST" | jq -r ".err_msg")
     CT_TRACKER_ID=$(echo ${ERROR_MESSAGE##* })
   else
-    CT_TRACKER_ID=$(echo "$CT_TRACKER_REQUEST" | jq ".results[].id")
+    CT_TRACKER_ID=$(echo "$CT_TRACKER_CREATE_REQUEST" | jq ".results[].id")
   fi
 fi
 echo "CT_TRACKER_ID: '${CT_TRACKER_ID}'"
