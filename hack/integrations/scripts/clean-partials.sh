a0a29dc4add30127e19163534c483c14c5e27fa3
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-10-06 16:49:48 -0700

    Don't fail if remove couldn't find file

--- hack/integrations/scripts/clean-partials.sh
+++ hack/integrations/scripts/clean-partials.sh
@@ -4,8 +4,8 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 source ${REPO_ROOT}/hack/test/deploy/k8s-utils.sh
 
 function main() {
-  rm $REPO_ROOT/hack/integrations/working/*.partial*
-  rm $REPO_ROOT/hack/integrations/working/*-partial*
+  rm -f $REPO_ROOT/hack/integrations/working/*.partial*
+  rm -f $REPO_ROOT/hack/integrations/working/*-partial*
 }
 
 main $@
