0777346487c29ae0955cd8e293ea8f1aeab84f8c
mamichael <mamichael@vmware.com>
2022-10-19 09:07:24 -0600

    Fix typo in clean-up script

--- hack/test/clean-cluster.sh
+++ hack/test/clean-cluster.sh
@@ -6,7 +6,7 @@ echo "Cleaning up cluster"
 CLUSTER_ROLES=$(kubectl get clusterroles | awk '/wavefront-collector|wavefront|wavefront-wavefront-collector|wavefront-wavefront-legacy-install-detection|wavefront-wavefront-logging/ {print $1}')
 if [[ ! -z "$CLUSTER_ROLES" ]] ; then
     echo "Found ClusterRoles: ${CLUSTER_ROLES}"
-		kubectl delete --wait=false clusterroles ${CLUSTER_ROLES} || true
+    kubectl delete --wait=false clusterroles ${CLUSTER_ROLES} || true
 	  kubectl delete --wait=false clusterrolebindings ${CLUSTER_ROLES} || true
 fi
 
