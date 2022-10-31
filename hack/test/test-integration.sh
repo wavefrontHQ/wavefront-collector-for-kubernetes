2cae15303daba9309cec255cdc656f1835e20490
Matthew Conger-Eldeen <matthewco@vmware.com>
2022-10-31 15:03:48 -0600

    put back integration tests

--- hack/test/test-integration.sh
+++ hack/test/test-integration.sh
@@ -52,64 +52,64 @@ kubectl apply -f ./deploy/mysql-config.yaml
 kubectl apply -f ./deploy/memcached-config.yaml
 kubectl config set-context --current --namespace=default
 
-#echo "deploying collector $IMAGE_NAME $VERSION"
-#
-#env USE_TEST_PROXY=true ./deploy.sh -c "$WAVEFRONT_CLUSTER" -t "$API_TOKEN" -v "$VERSION"  -k "$K8S_ENV" -n "$WF_CLUSTER_NAME" -e "$EXPERIMENTAL_FEATURES" -y "$COLLECTOR_YAML"
-#
-#wait_for_cluster_ready
-#
-#kubectl --namespace "$NS" port-forward deploy/wavefront-proxy 8888 &
-#trap 'kill $(jobs -p)' EXIT
-#
-#echo "waiting for logs..."
-#sleep ${SLEEP_TIME}
-#
-#DIR=$(dirname "$0")
-#RES=$(mktemp)
-#
-#if [ -f "overlays/test-$K8S_ENV/metrics/${METRICS_FILE_NAME}.jsonl" ]; then
-#  cat "files/${METRICS_FILE_NAME}.jsonl" overlays/test-$K8S_ENV/metrics/${METRICS_FILE_NAME}.jsonl  > files/combined-metrics.jsonl
-#else
-#  cat "files/${METRICS_FILE_NAME}.jsonl" > files/combined-metrics.jsonl
-#fi
-#
-#while true ; do # wait until we get a good connection
-#  RES_CODE=$(curl --silent --output "$RES" --write-out "%{http_code}" --data-binary "@$DIR/files/combined-metrics.jsonl" "http://localhost:8888/metrics/diff")
-#  [[ $RES_CODE -lt 200 ]] || break
-#done
-#
-#if [[ $RES_CODE -gt 399 ]] ; then
-#  red "INVALID METRICS"
-#  jq -r '.[]' "${RES}"
-#  exit 1
-#fi
-#
-#DIFF_COUNT=$(jq "(.Missing | length) + (.Unwanted | length)" "$RES")
-#EXIT_CODE=0
-#
-#jq -c '.Missing[]' "$RES" | sort > missing.jsonl
-#jq -c '.Extra[]' "$RES" | sort > extra.jsonl
-#jq -c '.Unwanted[]' "$RES" | sort > unwanted.jsonl
-#
-#echo "$RES"
-#if [[ $DIFF_COUNT -gt 0 ]] ; then
-#  red "Missing: $(jq "(.Missing | length)" "$RES")"
-#  if [[ $(jq "(.Missing | length)" "$RES") -le 10 ]]; then
-#    cat missing.jsonl
-#  fi
-#  red "Unwanted: $(jq "(.Unwanted | length)" "$RES")"
-#  if [[ $(jq "(.Unwanted | length)" "$RES") -le 10 ]]; then
-#    cat unwanted.jsonl
-#  fi
-#  red "Extra: $(jq "(.Extra | length)" "$RES")"
-#  red "FAILED: METRICS OUTPUT DID NOT MATCH"
-#  if which pbcopy > /dev/null; then
-#    echo "$RES" | pbcopy
-#  fi
-#  EXIT_CODE=1
-#else
-#  green "SUCCEEDED"
-#fi
+echo "deploying collector $IMAGE_NAME $VERSION"
+
+env USE_TEST_PROXY=true ./deploy.sh -c "$WAVEFRONT_CLUSTER" -t "$API_TOKEN" -v "$VERSION"  -k "$K8S_ENV" -n "$WF_CLUSTER_NAME" -e "$EXPERIMENTAL_FEATURES" -y "$COLLECTOR_YAML"
+
+wait_for_cluster_ready
+
+kubectl --namespace "$NS" port-forward deploy/wavefront-proxy 8888 &
+trap 'kill $(jobs -p)' EXIT
+
+echo "waiting for logs..."
+sleep ${SLEEP_TIME}
+
+DIR=$(dirname "$0")
+RES=$(mktemp)
+
+if [ -f "overlays/test-$K8S_ENV/metrics/${METRICS_FILE_NAME}.jsonl" ]; then
+  cat "files/${METRICS_FILE_NAME}.jsonl" overlays/test-$K8S_ENV/metrics/${METRICS_FILE_NAME}.jsonl  > files/combined-metrics.jsonl
+else
+  cat "files/${METRICS_FILE_NAME}.jsonl" > files/combined-metrics.jsonl
+fi
+
+while true ; do # wait until we get a good connection
+  RES_CODE=$(curl --silent --output "$RES" --write-out "%{http_code}" --data-binary "@$DIR/files/combined-metrics.jsonl" "http://localhost:8888/metrics/diff")
+  [[ $RES_CODE -lt 200 ]] || break
+done
+
+if [[ $RES_CODE -gt 399 ]] ; then
+  red "INVALID METRICS"
+  jq -r '.[]' "${RES}"
+  exit 1
+fi
+
+DIFF_COUNT=$(jq "(.Missing | length) + (.Unwanted | length)" "$RES")
+EXIT_CODE=0
+
+jq -c '.Missing[]' "$RES" | sort > missing.jsonl
+jq -c '.Extra[]' "$RES" | sort > extra.jsonl
+jq -c '.Unwanted[]' "$RES" | sort > unwanted.jsonl
+
+echo "$RES"
+if [[ $DIFF_COUNT -gt 0 ]] ; then
+  red "Missing: $(jq "(.Missing | length)" "$RES")"
+  if [[ $(jq "(.Missing | length)" "$RES") -le 10 ]]; then
+    cat missing.jsonl
+  fi
+  red "Unwanted: $(jq "(.Unwanted | length)" "$RES")"
+  if [[ $(jq "(.Unwanted | length)" "$RES") -le 10 ]]; then
+    cat unwanted.jsonl
+  fi
+  red "Extra: $(jq "(.Extra | length)" "$RES")"
+  red "FAILED: METRICS OUTPUT DID NOT MATCH"
+  if which pbcopy > /dev/null; then
+    echo "$RES" | pbcopy
+  fi
+  EXIT_CODE=1
+else
+  green "SUCCEEDED"
+fi
 
 env USE_TEST_PROXY=false ./deploy.sh -c "$WAVEFRONT_CLUSTER" -t "$API_TOKEN" -v "$VERSION"  -k "$K8S_ENV" -n "$WF_CLUSTER_NAME" -e "$EXPERIMENTAL_FEATURES" -y "$COLLECTOR_YAML"
 
