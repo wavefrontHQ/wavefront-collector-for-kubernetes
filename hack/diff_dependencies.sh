f6b720eb591d148f3bd3a5bfb40de44faedb4724
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-11-04 11:52:48 -0700

    debug

--- hack/diff_dependencies.sh
+++ hack/diff_dependencies.sh
@@ -68,7 +68,7 @@ EOF
   echo "./$SCRIPT_DIR/../osspi/tasks/osspi/"
   . ./$SCRIPT_DIR/../osspi/tasks/osspi/scan-source.sh
 
-
+  echo did we get here
   GOOS=linux go list -mod=readonly -deps -f '{{ if and (.DepOnly) (.Module) (not .Standard) }}{{ $mod := (or .Module.Replace .Module) }}{{ $mod.Path }}{{ end }}' ./... | grep -v $REPO | sort -u > $TEMP_DIR/from_go_mod.txt
   grep '   >>> ' open_source_licenses.txt | grep -v Apache | grep -v Mozilla | awk '{print $2}' | rev | awk -F'v-' '{print $2}' | rev | sort -u > $TEMP_DIR/from_open_source_licenses.txt
 
