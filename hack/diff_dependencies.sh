ab6a07c20bda4e9d67624ad13c510df8b72d87da
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-11-04 18:16:10 -0700

    get jq

--- hack/diff_dependencies.sh
+++ hack/diff_dependencies.sh
@@ -41,6 +41,12 @@ function main() {
     echo "osspi already installed: $($HOME/.osspicli/osspi/osspi --version)"
   fi
 
+  if ! [ -x "$(command -v jq)" ]; then
+    curl -H "Authorization: token ${GITHUB_CREDS_PSW}" -L "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" > ./jq
+    chmod +x ./jq
+    sudo mv ./jq /usr/local/bin
+  fi
+
   OSSPI_SCANNING_PARAMS=$(cat <<EOF
   enable: true
   include_bomtools: "go_mod"
