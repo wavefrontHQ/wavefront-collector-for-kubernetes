1bd09340df32812a658f3092c3920d489ce03245
Jerry Belmonte <bjerry@vmware.com>
2022-11-14 12:15:19 -0700

    Add ability to clone from a template dashboard
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>

--- hack/integrations/scripts/start-dashboard-development.sh
+++ hack/integrations/scripts/start-dashboard-development.sh
@@ -4,11 +4,12 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 source ${REPO_ROOT}/hack/test/deploy/k8s-utils.sh
 
 function print_usage_and_exit() {
-  echo "Failure: $1"
+  red "Failure: $1"
   echo "Usage: $0 [flags] [options]"
   echo -e "\t-c wavefront instance name (default: 'nimba')"
   echo -e "\t-t wavefront token (required)"
-  echo -e "\t-d dashboard url (required)"
+  echo -e "\t-d dashboard url to clone from (optional, if not given assumed to be template dashboard)"
+  echo -e "\t-n new dashboard url to create (required)"
   exit 1
 }
 
@@ -17,12 +18,14 @@ function main() {
 
   # REQUIRED
   local WAVEFRONT_TOKEN=
-  local DASHBOARD_URL=
+  local NEW_DASHBOARD=
+
+  # OPTIONAL
+  local DASHBOARD_TO_CLONE=integration-dashboard-template
 
   local WF_CLUSTER=nimba
 
-
-  while getopts ":c:t:d:" opt; do
+  while getopts ":c:t:d:n:" opt; do
     case $opt in
     c)
       WF_CLUSTER="$OPTARG"
@@ -31,7 +34,10 @@ function main() {
       WAVEFRONT_TOKEN="$OPTARG"
       ;;
     d)
-      DASHBOARD_URL="$OPTARG"
+      DASHBOARD_TO_CLONE="$OPTARG"
+      ;;
+    n)
+      NEW_DASHBOARD="$OPTARG"
       ;;
     \?)
       print_usage_and_exit "Invalid option: -$OPTARG"
@@ -40,41 +46,39 @@ function main() {
   done
 
   if [[ -z ${WAVEFRONT_TOKEN} ]]; then
-    print_msg_and_exit "wavefront token required"
+    print_usage_and_exit "wavefront token required"
   fi
 
-  if [[ -z ${DASHBOARD_URL} ]]; then
-    print_msg_and_exit "dashboard url required"
+  if [[ -z ${NEW_DASHBOARD} ]]; then
+    print_usage_and_exit "new dashboard url required"
   fi
 
-  local DASHBOARD_DEV_URL="$(echo "${DASHBOARD_URL}" | sed 's/integration-//')-dev"
+  ../scripts/get-dashboard.sh -c ${WF_CLUSTER} -t ${WAVEFRONT_TOKEN} -d ${DASHBOARD_TO_CLONE} -o ${DASHBOARD_TO_CLONE}-partial-base.json
 
-  ../scripts/get-dashboard.sh -c ${WF_CLUSTER} -t ${WAVEFRONT_TOKEN} -d ${DASHBOARD_URL} -o ${DASHBOARD_URL}-partial-base.json
-
-  ../scripts/sort-dashboard.sh -i ${DASHBOARD_URL}-partial-base.json -o ${DASHBOARD_URL}.json
+  ../scripts/sort-dashboard.sh -i ${DASHBOARD_TO_CLONE}-partial-base.json -o ${DASHBOARD_TO_CLONE}.json
 
   ../scripts/clean-partials.sh # because I don't want scripts bludgeoning the '-partial-base.json'
 
-  jq ".url = \"${DASHBOARD_DEV_URL}\"" ${DASHBOARD_URL}.json > ${DASHBOARD_DEV_URL}.json
+  jq ".url = \"${NEW_DASHBOARD}\"" ${DASHBOARD_TO_CLONE}.json > ${NEW_DASHBOARD}.json
 
-  local RESULT=$(curl -X PUT --data "$(cat "${DASHBOARD_DEV_URL}".json)" \
+  local RESULT=$(curl -X PUT --data "$(cat "${NEW_DASHBOARD}".json)" \
     --header "Content-Type: application/json" \
     --header "Authorization: Bearer ${WAVEFRONT_TOKEN}" \
-    "https://${WF_CLUSTER}.wavefront.com/api/v2/dashboard/${DASHBOARD_DEV_URL}" \
+    "https://${WF_CLUSTER}.wavefront.com/api/v2/dashboard/${NEW_DASHBOARD}" \
     --write-out '%{http_code}' --silent --output /dev/null)
   if [[ $RESULT -ne 200 ]]; then
-    RESULT=$(curl --silent -X POST --data "$(cat "${DASHBOARD_DEV_URL}".json)" \
+    RESULT=$(curl --silent -X POST --data "$(cat "${NEW_DASHBOARD}".json)" \
       --header "Content-Type: application/json" \
       --header "Authorization: Bearer ${WAVEFRONT_TOKEN}" \
       "https://${WF_CLUSTER}.wavefront.com/api/v2/dashboard" \
       --write-out '%{http_code}' --silent --output /dev/null)
     if [[ $RESULT -ne 200 ]]; then
-      red "Uploading ${DASHBOARD_DEV_URL} dashboard failed with error code: ${RESULT}"
+      red "Uploading ${NEW_DASHBOARD} dashboard failed with error code: ${RESULT}"
       exit 1
     fi
   fi
 
-  green "Dashboard uploaded at https://${WF_CLUSTER}.wavefront.com/dashboards/${DASHBOARD_DEV_URL}"
+  green "Dashboard uploaded at https://${WF_CLUSTER}.wavefront.com/dashboards/${NEW_DASHBOARD}"
 }
 
 main $@
