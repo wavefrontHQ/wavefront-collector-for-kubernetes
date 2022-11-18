415b68238e6413eee654170720fd00350c18fd27
Mark Michael <mamichael@vmware.com>
2022-11-18 15:20:28 -0700

    Add AKS integration test
    Signed-off-by: John Cornish <jcornish@vmware.com>
    Co-authored-by: John Cornish <jcornish@vmware.com>

--- hack/test/deploy/get-k8s-cluster-env.sh
+++ hack/test/deploy/get-k8s-cluster-env.sh
@@ -12,6 +12,10 @@ if grep -q "kind" <<< "$CURRENT_CONTEXT"; then
   echo "Kind"
 elif grep -q "gke" <<< "$CURRENT_CONTEXT"; then
   echo "GKE"
+elif grep -q "aks" <<< "$CURRENT_CONTEXT"; then
+  echo "AKS"
+elif grep -q "k8po-ci" <<< "$CURRENT_CONTEXT"; then
+  echo "AKS"
 elif grep -q "eks" <<< "$CURRENT_CONTEXT"; then
   echo "EKS"
 else
