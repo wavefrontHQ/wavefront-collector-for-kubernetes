399b67556ecfbbdc18952b4371d1e02d936ebaac
Anil Krishna Kodali <38334031+akodali18@users.noreply.github.com>
2022-12-05 11:32:34 -0700

    K8 ssaas 954 integration test improvements (#539)
    Co-authored-by: Jerry Belmonte <bjerry@vmware.com>
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>
    Co-authored-by: John Cornish <jcornish@vmware.com>

--- hack/test/generate.sh
+++ hack/test/generate.sh
@@ -1,124 +1,115 @@
 #!/bin/bash -e
-source ./deploy/k8s-utils.sh
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-
-cd "$(dirname "$0")" # hack/test
-
-DEFAULT_VERSION="1.10.0"
-USE_TEST_PROXY="${USE_TEST_PROXY:-false}"
-
-if [ "$USE_TEST_PROXY" = true ] ;
-  then
-    FLUSH_INTERVAL=18
-    COLLECTION_INTERVAL=7
-  else
-    FLUSH_INTERVAL=30
-    COLLECTION_INTERVAL=60
-fi
-
-function print_usage_and_exit() {
-    echo "Failure: $1"
-    echo "Usage: $0 [flags] [options]"
-    echo -e "\t-c wavefront instance name (required)"
-    echo -e "\t-t wavefront token (required)"
-    echo -e "\t-v collector docker image version"
-    echo -e "\t-k K8s ENV (required)"
-    echo -e "\t-n K8s Cluster name"
-    echo -e "\t-e experimental features"
-    echo -e "\t-y collector yaml"
-    exit 1
-}
-
-WF_CLUSTER=
-WAVEFRONT_TOKEN=
-VERSION=
-K8S_ENV=gke
-COLLECTOR_YAML=
-WF_CLUSTER_NAME=
-EXPERIMENTAL_FEATURES=
-
-while getopts "c:t:v:d:k:n:e:y:" opt; do
-  case $opt in
-    c)
-      WF_CLUSTER="$OPTARG"
-      ;;
-    t)
-      WAVEFRONT_TOKEN="$OPTARG"
-      ;;
-    v)
-      VERSION="$OPTARG"
-      ;;
-    k)
-      K8S_ENV="$OPTARG"
-      ;;
-    n)
-      WF_CLUSTER_NAME="$OPTARG"
-      ;;
-    e)
-      EXPERIMENTAL_FEATURES="$OPTARG"
-      ;;
-    y)
-      COLLECTOR_YAML="$OPTARG"
-      ;;
-    \?)
-      print_usage_and_exit "Invalid option: -$OPTARG"
-      ;;
-  esac
-done
-
-if [[ -z ${WF_CLUSTER} || -z ${WAVEFRONT_TOKEN} ]] ; then
-    #TODO: source these from environment variables if not provided
-    print_usage_and_exit "wavefront instance and token required"
-fi
-
-
-if [[ -z ${VERSION} ]] ; then
-    VERSION=${DEFAULT_VERSION}
-fi
-
-if [[ -z ${COLLECTOR_YAML} ]] ; then
-    COLLECTOR_YAML="${REPO_ROOT}/deploy/kubernetes/5-collector-daemonset.yaml"
-fi
-
-if [[ -z ${WF_CLUSTER_NAME} ]] ; then
-    WF_CLUSTER_NAME=$(whoami)-${K8S_ENV}-$(date +"%y%m%d")
-fi
-
-cp "${REPO_ROOT}/deploy/kubernetes/0-collector-namespace.yaml" base/deploy/0-collector-namespace.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/1-collector-cluster-role.yaml" base/deploy/1-collector-cluster-role.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/2-collector-rbac.yaml" base/deploy/2-collector-rbac.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/3-collector-service-account.yaml" base/deploy/3-collector-service-account.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/5-collector-daemonset.yaml" base/deploy/collector-deployments/5-collector-daemonset.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/alternate-collector-deployments/5-collector-single-deployment.yaml" base/deploy/collector-deployments/5-collector-single-deployment.yaml
-cp "${REPO_ROOT}/deploy/kubernetes/alternate-collector-deployments/5-collector-combined.yaml" base/deploy/collector-deployments/5-collector-combined.yaml
-
-csplit base/deploy/collector-deployments/5-collector-combined.yaml '/^---$/' &>/dev/null
-mv xx00 base/deploy/collector-deployments/5-collector-node-metrics-only.yaml
-mv xx01 base/deploy/collector-deployments/5-collector-cluster-metrics-only.yaml
-
-cp "${COLLECTOR_YAML}" base/deploy/5-wavefront-collector.yaml
-
-
+source "${REPO_ROOT}"/hack/test/deploy/k8s-utils.sh
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 NS=wavefront-collector
 
-if $USE_TEST_PROXY ; then
-  cp base/test-proxy.yaml base/deploy/6-wavefront-proxy.yaml
-else
-  sed "s/YOUR_CLUSTER/${WF_CLUSTER}/g; s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" base/proxy.template.yaml > base/deploy/6-wavefront-proxy.yaml
-fi
+function copy_collector_deployment_files() {
+  echo "Copying collector deployment files"
 
-# TODO: only sed into kustomization template and have it fill in variables in files
- sed "s/YOUR_IMAGE_TAG/${VERSION}/g" base/kustomization.template.yaml  > base/kustomization.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/0-collector-namespace.yaml" base/deploy/0-collector-namespace.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/1-collector-cluster-role.yaml" base/deploy/1-collector-cluster-role.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/2-collector-rbac.yaml" base/deploy/2-collector-rbac.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/3-collector-service-account.yaml" base/deploy/3-collector-service-account.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/5-collector-daemonset.yaml" base/deploy/collector-deployments/5-collector-daemonset.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/alternate-collector-deployments/5-collector-single-deployment.yaml" base/deploy/collector-deployments/5-collector-single-deployment.yaml
+  cp "${REPO_ROOT}/deploy/kubernetes/alternate-collector-deployments/5-collector-combined.yaml" base/deploy/collector-deployments/5-collector-combined.yaml
 
-sed "s/YOUR_CLUSTER_NAME/${WF_CLUSTER_NAME}/g"  base/collector-config.template.yaml  |
-  sed "s/NAMESPACE/${NS}/g" |
-  sed "s/YOUR_EXPERIMENTAL_FEATURES/${EXPERIMENTAL_FEATURES}/g" |
-  sed "s/FLUSH_INTERVAL/${FLUSH_INTERVAL}/g" |
-  sed  "s/COLLECTION_INTERVAL/${COLLECTION_INTERVAL}/g" > base/deploy/4-collector-config.yaml
+  csplit base/deploy/collector-deployments/5-collector-combined.yaml '/^---$/' &>/dev/null
+  mv xx00 base/deploy/collector-deployments/5-collector-node-metrics-only.yaml
+  mv xx01 base/deploy/collector-deployments/5-collector-cluster-metrics-only.yaml
 
+  cp "${COLLECTOR_YAML}" base/deploy/5-wavefront-collector.yaml
+}
 
-if  [ ${USE_TEST_PROXY} == false ] && [ "${VERSION}" != "fake" ]; then
-  echo "IMAGE TAG: ${VERSION}"
-  green "WF Cluster Name: ${WF_CLUSTER_NAME}"
-fi
+function replace_placeholders_in_template_yaml() {
+  echo "Replacing placeholders in template yaml files"
+  local FLUSH_INTERVAL=30
+  local COLLECTION_INTERVAL=60
+
+  if [[ "${USE_TEST_PROXY}" = "true" ]]; then
+    FLUSH_INTERVAL=18
+    COLLECTION_INTERVAL=7
+    cp base/test-proxy.yaml base/deploy/6-wavefront-proxy.yaml
+  else
+    sed -e "s/YOUR_CLUSTER/${WF_CLUSTER}/g" \
+        -e "s/YOUR_API_TOKEN/${WAVEFRONT_TOKEN}/g" \
+        base/proxy.template.yaml > base/deploy/6-wavefront-proxy.yaml
+  fi
+
+  sed "s/YOUR_IMAGE_TAG/${VERSION}/g" base/kustomization.template.yaml > base/kustomization.yaml
+
+  sed -e "s/NAMESPACE/${NS}/g" \
+      -e "s/YOUR_CLUSTER_NAME/${K8S_CLUSTER_NAME}/g" \
+      -e "s/YOUR_EXPERIMENTAL_FEATURES/${EXPERIMENTAL_FEATURES}/g" \
+      -e "s/FLUSH_INTERVAL/${FLUSH_INTERVAL}/g" \
+      -e "s/COLLECTION_INTERVAL/${COLLECTION_INTERVAL}/g" \
+      base/collector-config.template.yaml > base/deploy/4-collector-config.yaml
+}
+
+function print_usage_and_exit() {
+  red "Failure: $1"
+  echo "Usage: $0 -c <WAVEFRONT_CLUSTER> -t <WAVEFRONT_TOKEN> -v [VERSION] -k [K8S_ENV] -n [K8S_CLUSTER_NAME] -y [COLLECTOR_YAML] -p [USE_TEST_PROXY]"
+  echo "  -c wavefront instance name (required)"
+  echo "  -t wavefront token (required)"
+  echo "  -v collector docker image version (default: load from 'release/VERSION')"
+  echo "  -k K8s ENV"
+  echo "  -n K8s Cluster name"
+  echo "  -y collector yaml"
+  echo "  -p use test proxy (default: 'false')"
+  echo "  -e experimental features"
+  exit 1
+}
+
+function check_required_argument() {
+  local required_arg=$1
+  local failure_msg=$2
+  if [[ -z ${required_arg} ]]; then
+    print_usage_and_exit "$failure_msg"
+  fi
+}
+
+function main() {
+  cd "${SCRIPT_DIR}" # hack/test
+
+  # REQUIRED
+  local WF_CLUSTER=
+  local WAVEFRONT_TOKEN=
+
+  # OPTIONAL/DEFAULT
+  local VERSION="$(cat "${REPO_ROOT}"/release/VERSION)"
+  local K8S_ENV=$("${SCRIPT_DIR}"/deploy/get-k8s-cluster-env.sh | awk '{print tolower($0)}' )
+  local K8S_CLUSTER_NAME=$(whoami)-${K8S_ENV}-$(date +"%y%m%d")
+  local COLLECTOR_YAML="${REPO_ROOT}/deploy/kubernetes/5-collector-daemonset.yaml"
+  local USE_TEST_PROXY="false"
+  local EXPERIMENTAL_FEATURES=
+
+  while getopts "c:t:v:k:n:e:y:p:" opt; do
+    case $opt in
+      c)  WF_CLUSTER="$OPTARG" ;;
+      t)  WAVEFRONT_TOKEN="$OPTARG" ;;
+      v)  VERSION="$OPTARG" ;;
+      k)  K8S_ENV="$OPTARG" ;;
+      n)  K8S_CLUSTER_NAME="$OPTARG" ;;
+      y)  COLLECTOR_YAML="$OPTARG" ;;
+      p)  USE_TEST_PROXY="$OPTARG" ;;
+      e)  EXPERIMENTAL_FEATURES="$OPTARG" ;;
+      \?) print_usage_and_exit "Invalid option: -$OPTARG" ;;
+    esac
+  done
+
+  check_required_argument "$WF_CLUSTER" "-c <WAVEFRONT_CLUSTER> is required"
+  check_required_argument "$WAVEFRONT_TOKEN" "-t <WAVEFRONT_TOKEN> is required"
+
+  echo "Generating deployment files for wavefront collector"
+  copy_collector_deployment_files
+  replace_placeholders_in_template_yaml
+
+  if [[ "${USE_TEST_PROXY}" = "true" ]] && [[ "${VERSION}" != "fake" ]]; then
+    echo "IMAGE TAG: ${VERSION}"
+    green "WF Cluster Name: ${K8S_CLUSTER_NAME}"
+  fi
+}
+
+main $@
