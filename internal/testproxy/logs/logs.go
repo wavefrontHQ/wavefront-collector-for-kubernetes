ed04e7d641ab553e1ad127530d831033dbb01bcd
mamichael <mamichael@vmware.com>
2023-01-03 10:19:12 -0700

    WIP
    Co-authored-by: Jeremy Alvis <jalvis@vmware.com>

--- internal/testproxy/logs/logs.go
+++ internal/testproxy/logs/logs.go
@@ -3,26 +3,61 @@ package logs
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+)
+
+var (
+	expectedTags = [...]string{"service", "application", "source", "cluster", "timestamp", "pod_name", "container_name", "namespace_name", "pod_id", "container_id"}
 )
 
 func VerifyJsonArray(line string) bool {
-	var jsonarray []interface{}
-	err := json.Unmarshal([]byte(line), &jsonarray)
+	var logLines []interface{}
+	err := json.Unmarshal([]byte(line), &logLines)
 	if err != nil {
 		fmt.Println("Data is not in json array format:", err)
 		return false
 	}
 
+	for _, logLine := range logLines {
+		if validateLogLine(logLine) != true {
+			return false
+		}
+	}
 	return true
 }
 
 func VerifyJsonLines(line string) bool {
-	var jsonlines []interface{}
-	err := json.Unmarshal([]byte(line), &jsonlines)
-	if err == nil {
-		fmt.Println("Data is not in json lines format :", err)
-		return false
+	decoder := json.NewDecoder(strings.NewReader(line))
+	for decoder.More() {
+		var jsonLine interface{}
+		if err := decoder.Decode(&jsonLine); err != nil {
+			fmt.Println("Data is not in json line format:", err)
+			return false
+		}
 	}
 
+	return true
+}
+
+func validateLogLine(logLine interface{}) bool {
+	//return validateTags(logLine)
+	return true
+}
+
+func validateTags(logLine interface{}) bool {
+	myMap := logLine.(map[string]interface{})
+
+	for _, expectedTag := range expectedTags {
+		if val, ok := myMap[expectedTag]; ok {
+			if len(val.(string)) == 0 {
+				fmt.Printf("Empty expected tag: %s\n", expectedTag)
+				return false
+			}
+		} else {
+			fmt.Printf("Missing expected tag: %s\n", expectedTag)
+			return false
+		}
+
+	}
 	return true
 }
--- internal/testproxy/logs/logs_test.go
+++ internal/testproxy/logs/logs_test.go
@@ -1,6 +1,8 @@
 package logs_test
 
 import (
+	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 
 	"github.com/wavefronthq/wavefront-collector-for-kubernetes/internal/testproxy/logs"
@@ -8,16 +10,39 @@ import (
 
 func TestLogFormat(t *testing.T) {
 	t.Run("logs are in json_array format", func(t *testing.T) {
-		jsonArray := `[
-					{"cluster":"testk8scluster","message":"testlogmessage","service":"none"},
-					{"cluster":"testk8scluster","message":"testlogmessage","service":"none"}
-					]`
-		logs.VerifyJsonArray(jsonArray)
+		require.True(t, logs.VerifyJsonArray(validJsonArray()))
+	})
+
+	t.Run("logs are not in json_array format", func(t *testing.T) {
+		jsonArray := `{"cluster":"testk8scluster","message":"testlogmessage","service":"none"}
+					{"cluster":"testk8scluster","message":"testlogmessage","service":"none"}`
+		require.False(t, logs.VerifyJsonArray(jsonArray))
 	})
 
 	t.Run("logs are in json lines format", func(t *testing.T) {
 		jsonLines := `{"cluster":"testk8scluster","message":"testlogmessage","service":"none"}
-					{"cluster":"testk8scluster","message":"testlogmessage","service":"none"}`
-		logs.VerifyJsonLines(jsonLines)
+                      {"cluster":"testk8scluster","message":"testlogmessage","service":"none"}`
+		require.True(t, logs.VerifyJsonLines(jsonLines))
 	})
+
+	t.Run("logs are not in json lines format", func(t *testing.T) {
+		jsonLines := `{"cluster":"testk8scluster","message":"testlogmessage","service":"none"},
+                      {"cluster":"testk8scluster","message":"testlogmessage","service":"none"}`
+		require.False(t, logs.VerifyJsonLines(jsonLines))
+	})
+}
+
+func TestLogTags(t *testing.T) {
+	t.Run("logs have required tags", func(t *testing.T) {
+		jsonArray := "[" + validLogLine() + "]"
+		require.True(t, logs.VerifyJsonArray(jsonArray))
+	})
+}
+
+func validJsonArray() string {
+	return "[" + strings.Join([]string{validLogLine(), validLogLine()}, ",\n") + "]"
+}
+
+func validLogLine() string {
+	return "{\"cluster\":\"testk8scluster\",\"message\":\"testlogmessage\",\"service\":\"none\"}"
 }
